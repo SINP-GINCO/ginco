f1cf5e17b8897a0e7cd1dfdbd4512325b41c3e93
Anna Mouget <anna.mouget@ign.fr>
2016-10-05 14:28:53 +0200

    #836: Replace ogam by ginco in page title

---
+++ website/htdocs/custom/client/ogamDesktop/overrides/ogamDesktop/app/view/main/Main.js
@@ -0,0 +1,18 @@
+/**
+ * This class is the main view for the application. It is specified in app.js as the
+ * "autoCreateViewport" property. That setting automatically applies the "viewport"
+ * plugin to promote that instance of this class to the body element.
+ */
+Ext.define('Ginco.view.main.Main', {
+	extend: 'OgamDesktop.view.main.Main',
+
+	/**
+	 * Initializes the component.
+	 */
+	initComponent : function() {
+		// Customize the title of the html page, defined in client/ogamDesktop/index.html
+		// for the other pages it's made in translations files.
+		document.title = 'GINCO';
+		this.callParent(arguments);
+	}
+});
