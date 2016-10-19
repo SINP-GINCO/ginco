55ee3bda34f1491d1f378842da91288d7ef9b4ca
Anna Mouget <anna.mouget@ign.fr>
2016-10-19 10:39:08 +0200

    #853: Add help button in visu page

--- website/htdocs/custom/client/ogamDesktop/overrides/ogamDesktop/app/view/main/Main.js
+++ website/htdocs/custom/client/ogamDesktop/overrides/ogamDesktop/app/view/main/Main.js
@@ -1,18 +1,38 @@
 /**
- * This class is the main view for the application. It is specified in app.js as the
- * "autoCreateViewport" property. That setting automatically applies the "viewport"
- * plugin to promote that instance of this class to the body element.
+ * This class is the main view for the application. It is specified in app.js as
+ * the "autoCreateViewport" property. That setting automatically applies the
+ * "viewport" plugin to promote that instance of this class to the body element.
  */
 Ext.define('Ginco.view.main.Main', {
-	override: 'OgamDesktop.view.main.Main',
+	override : 'OgamDesktop.view.main.Main',
 
 	/**
 	 * Initializes the component.
 	 */
 	initComponent : function() {
-		// Customize the title of the html page, defined in client/ogamDesktop/index.html
+		// Customize the title of the html page, defined in
+		// client/ogamDesktop/index.html
 		// for the other pages it's made in translations files.
 		document.title = 'GINCO';
+
+		// Add help button
+		this.getTabBar().add({
+			xtype : 'tbfill'
+		},{
+			xtype : 'button',
+			cls : 'home-button',
+			iconCls : 'o-help-icone',
+			ui : 'default-toolbar',
+			text : 'Aide',
+			tooltip : 'Ouvrir l\'aide',
+			handler : function() {
+				window.open('https://ginco.ign.fr/doc', '_blank');
+			}
+		},{
+			xtype:'tbspacer',
+			width: 10
+		});
+
 		this.callParent(arguments);
 	}
 });
