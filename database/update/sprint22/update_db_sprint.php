0bef8075e53362c6d058b4c37359bdc5e4046421
Severine <severine.candelier@gmail.com>
2016-11-21 10:43:52 +0100

    #742: Rename test instances in application_parameters

--- database/update/sprint22/update_db_sprint.php
+++ database/update/sprint22/update_db_sprint.php
@@ -27,6 +27,7 @@ $config = loadPropertiesFromArgs();
 try {
 	/* patch code here */
     execCustSQLFile("$sprintDir/update_region_names.sql", $config);
+    execCustSQLFile("$sprintDir/update_region_names_in_application_parameters.sql", $config);
     execCustSQLFile("$sprintDir/add_site_name.sql", $config);
     execCustSQLFile("$sprintDir/change_codeME_to_array.sql", $config);
     execCustSQLFile("$sprintDir/change_nomCommuneCalcule_to_ARRAY_of_STRING.sql", $config);
