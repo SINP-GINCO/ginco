87c6c41f4c939b45f492b8ae3848767f6d6e7e4a
Anna Mouget <anna.mouget@ign.fr>
2016-11-22 17:23:34 +0100

    #950: Set email mandatory in user form

--- website/htdocs/custom/server/ogamServer/app/controllers/UsermanagementController.php
+++ website/htdocs/custom/server/ogamServer/app/controllers/UsermanagementController.php
@@ -20,6 +20,154 @@ include_once APPLICATION_PATH . '/controllers/UsermanagementController.php';
 class Custom_UsermanagementController extends UsermanagementController {
 
 	/**
+	 * Build and return the user form.
+	 *
+	 * @param
+	 *        	String the mode of the form ('create' or 'edit')
+	 * @param
+	 *        	Application_Object_Website_User the user
+	 * @return a Zend Form
+	 */
+	protected function getUserForm($mode = null, $user = null) {
+		$form = new Application_Form_OGAMForm(array(
+			'attribs' => array(
+				'name' => 'user-form',
+				'action' => $this->baseUrl . '/usermanagement/validate-user'
+			)
+		));
+		
+		//
+		// Add the login element:
+		//
+		$login = $form->createElement('text', 'login');
+		$login->setLabel('Login');
+		$login->addValidator('alnum');
+		$login->addValidator('regex', false, array(
+			'/^[a-z]+/'
+		));
+		$login->addValidator('stringLength', false, array(
+			2,
+			20
+		));
+		$login->setRequired(true);
+		$login->addFilter('StringToLower');
+		if ($user != null) {
+			$login->setValue($user->login);
+		}
+		
+		if ($mode == 'edit') {
+			$login->setAttrib('readonly', 'true');
+		} else {
+			$login->addValidator(new Application_Validator_UserNotExist());
+		}
+		
+		//
+		// Add the password elements
+		//
+		if ($mode == 'create') {
+			// Create and configure password element
+			$password = $form->createElement('password', 'password');
+			$password->addValidator(new Application_Validator_PasswordConfirmation());
+			$password->setLabel('Password');
+			$password->setRequired(true);
+			$password->setDescription("Votre mot de passe doit contenir au moins 8 caractères, une majuscule, une miniscule, un chiffre et un caractère spécial.");
+			$password->addValidator('regex', false, array(
+				'pattern' => '/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[$@$!%*?&])[A-Za-z\d$@$!%*?&]{8,}/',
+				'messages' => $this->translator->translate('InvalidPassword')
+			));
+			
+			$confirmPassword = $form->createElement('password', 'confirmPassword');
+			$confirmPassword->setLabel('Confirm Password');
+			$confirmPassword->setRequired(true);
+		}
+		
+		//
+		// Add the user name element
+		//
+		$usernameElem = $form->createElement('text', 'username');
+		$usernameElem->setLabel('User Name');
+		$usernameElem->setRequired(true);
+		if ($user != null) {
+			$usernameElem->setValue($user->username);
+		}
+		
+		//
+		// Add the provider element
+		//
+		$providerIdElem = $form->createElement('select', 'providerId');
+		$providerIdElem->setLabel('Provider');
+		$providerIdElem->setRequired(true);
+		if ($user != null && $user->provider != null) {
+			$providerIdElem->setValue($user->provider->id);
+		}
+		$providers = $this->providerModel->getProvidersList();
+		$providersChoices = array();
+		foreach ($providers as $provider) {
+			$providersChoices[$provider->id] = $provider->label;
+		}
+		$providerIdElem->addMultiOptions($providersChoices);
+		
+		//
+		// Add the email element
+		//
+		$emailElem = $form->createElement('text', 'email');
+		$emailElem->setLabel('Email');
+		$emailElem->setRequired(true);
+		if ($user != null && $user->email != null) {
+			$emailElem->setValue($user->email);
+		}
+		$emailElem->addValidator('EmailAddress');
+		
+		//
+		// Add the roles element
+		//
+		$roleCodeElem = $form->createElement('multiCheckbox', 'rolesCodes');
+		$roleCodeElem->setLabel('Roles');
+		$roleCodeElem->setRequired(true);
+		$allroles = $this->roleModel->getRolesList();
+		$options = array();
+		foreach ($allroles as $role) {
+			$options[$role->code] = $role->label;
+		}
+		$roleCodeElem->addMultiOptions($options);
+		
+		if ($user != null) {
+			$userRoles = array();
+			foreach ($user->rolesList as $role) {
+				$userRoles[] = $role->code;
+			}
+			$roleCodeElem->setValue($userRoles);
+		}
+		
+		//
+		// Create the submit button
+		//
+		$submitElement = $form->createElement('submit', 'submit');
+		$submitElement->setLabel('Submit');
+		
+		//
+		// Create a hidden mode element
+		//
+		$modeElement = $form->createElement('hidden', 'mode');
+		$modeElement->setValue($mode);
+		
+		// Add elements to form:
+		$form->addElement($login);
+		if ($mode == 'create') {
+			$form->addElement($password);
+			$form->addElement($confirmPassword);
+		}
+		$form->addElement($usernameElem);
+		$form->addElement($providerIdElem);
+		$form->addElement($emailElem);
+		$form->addElement($roleCodeElem);
+		$form->addElement($modeElement);
+		$form->addElement($submitElement);
+		
+		return $form;
+	}
+
+	/**
 	 * Build and return the role form.
 	 *
 	 * @param String $mode
@@ -35,7 +183,7 @@ class Custom_UsermanagementController extends UsermanagementController {
 				'action' => $this->baseUrl . '/usermanagement/validate-role'
 			)
 		));
-
+		
 		//
 		// Add the role code
 		//
@@ -48,7 +196,7 @@ class Custom_UsermanagementController extends UsermanagementController {
 		if ($mode == 'edit') {
 			$roleCode->setAttrib('readonly', 'true');
 		}
-
+		
 		//
 		// Add the role label
 		//
@@ -58,7 +206,7 @@ class Custom_UsermanagementController extends UsermanagementController {
 		if ($role != null) {
 			$roleLabel->setValue($role->label);
 		}
-
+		
 		//
 		// Add the role definition
 		//
@@ -68,7 +216,7 @@ class Custom_UsermanagementController extends UsermanagementController {
 		if ($role != null) {
 			$roleDefinition->setValue($role->definition);
 		}
-
+		
 		// Permissions
 		// Get all the Permissions
 		$allpermissions = $this->roleModel->getAllPermissions();
@@ -79,19 +227,19 @@ class Custom_UsermanagementController extends UsermanagementController {
 		if ($role != null) {
 			$rolepermissions->setValue($role->permissionsList); // set the selected permissions
 		}
-
+		
 		//
 		// Create the submit button
 		//
 		$submitElement = $form->createElement('submit', 'submit');
 		$submitElement->setLabel('Submit');
-
+		
 		//
 		// Create a hidden mode element
 		//
 		$modeElement = $form->createElement('hidden', 'mode');
 		$modeElement->setValue($mode);
-
+		
 		// Add elements to form:
 		$form->addElement($roleCode);
 		$form->addElement($roleLabel);
@@ -99,7 +247,7 @@ class Custom_UsermanagementController extends UsermanagementController {
 		$form->addElement($modeElement);
 		$form->addElement($rolepermissions);
 		$form->addElement($submitElement);
-
+		
 		return $form;
 	}
 
@@ -110,17 +258,17 @@ class Custom_UsermanagementController extends UsermanagementController {
 	 */
 	public function validateRoleAction() {
 		$this->logger->debug('validateRole');
-
+		
 		// Check the validity of the POST
 		if (!$this->getRequest()->isPost()) {
 			$this->logger->debug('form is not a POST');
 			return $this->_forward('index');
 		}
-
+		
 		// Check the validity of the form
 		$mode = $_POST['mode'];
 		$form = $this->getRoleForm($mode);
-
+		
 		if (!$form->isValid($_POST)) {
 			// Failed validation; redisplay form
 			$this->logger->debug('form is not valid');
@@ -147,17 +295,17 @@ class Custom_UsermanagementController extends UsermanagementController {
 			}
 			
 			$values = $form->getValues();
-
+			
 			$f = new Zend_Filter_StripTags();
 			$roleCode = $f->filter($values['roleCode']);
 			$roleLabel = $f->filter($values['roleLabel']);
 			$roleDefinition = $f->filter($values['roleDefinition']);
 			$rolepermissions = $values['rolepermissions'];
-
+			
 			// schema : we add RAW_DATA
 			$schemasList = array();
 			$schemasList['RAW_DATA'] = 'RAW_DATA';
-
+			
 			// Build the user
 			$role = new Application_Object_Website_Role();
 			$role->code = $roleCode;
@@ -165,23 +313,23 @@ class Custom_UsermanagementController extends UsermanagementController {
 			$role->definition = $roleDefinition;
 			$role->permissionsList = $rolepermissions;
 			$role->schemasList = $schemasList;
-
+			
 			if ($mode == 'edit') {
 				//
 				// EDIT the role
 				//
-
+				
 				// Update the user in database
 				$this->roleModel->updateRole($role);
 			} else {
 				//
 				// CREATE the new role
 				//
-
+				
 				// Create the user in database
 				$this->roleModel->createRole($role);
 			}
-
+			
 			// Return to the user list page
 			$this->showRolesAction();
 		}
