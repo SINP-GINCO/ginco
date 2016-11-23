bb82408058042b72742e0e10e9129a563a6302f1
Severine <severine.candelier@gmail.com>
2016-11-23 16:40:55 +0100

    #644: Change filename and filepath for DEE export file

---
+++ database/update/sprint23/insert_dee_gml_parameters.sql
@@ -0,0 +1,4 @@
+INSERT INTO website.application_parameters(name, value, description)
+VALUES ('deePrivateDirectory','/var/www/localhost/website/dee','Directory where DEE GML files are generated and stored'),
+       ('regionCode','@region.code@','INSEE Code for region, or ISO code for country')
+;
