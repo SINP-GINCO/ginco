d30d1347d64d4fb39fee5498ce7d85d700200a0a
Severine <severine.candelier@gmail.com>
2016-11-24 15:37:55 +0100

    #644:  Increase columns length in db for DEE export

--- database/update/sprint23/update_db_sprint.php
+++ database/update/sprint23/update_db_sprint.php
@@ -26,6 +26,7 @@ $config = loadPropertiesFromArgs();
 
 try {
     /* patch code here */
+    execCustSQLFile("$sprintDir/increase_columns_lengths_for_dee_export.sql", $config);
     execCustSQLFile("$sprintDir/insert_dee_gml_parameters.sql", $config);
 } catch(Exception $e){
 	echo "$sprintDir/update_db_sprint.php\n";
