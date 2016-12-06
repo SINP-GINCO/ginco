0a023ea34ace99413c7cd66d8305b3d6be6a5126
Severine <severine.candelier@gmail.com>
2016-12-06 15:34:27 +0100

    Fix logging of the Mailer Service

--- website/htdocs/custom/server/ogamServer/app/services/MailerService.php
+++ website/htdocs/custom/server/ogamServer/app/services/MailerService.php
@@ -69,7 +69,7 @@ class Application_Service_MailerService
             try {
                 $this->mailer->send($message);
                 $this->logger->debug("Email sent: \n" .
-                    "To: " . implode(',',$message->getTo()) . "\n" .
+                    "To: " . implode(',',array_keys($message->getTo())) . "\n" .
                     "Title: " . $message->getSubject() . "\n" .
                     "Body: " . $message->getBody() ."\n"
                 );
