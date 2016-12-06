43c9465e17adc9be43ff60fd9a2b5c7ed8a04598
Gautam <gautam.pastakia@ign.fr>
2016-12-06 14:33:51 +0100

    #600: Forbid access to new model function

---
+++ src/Ign/Bundle/GincoConfigurateurBundle/Controller/ModelController.php
@@ -0,0 +1,22 @@
+<?php
+namespace Ign\Bundle\GincoConfigurateurBundle\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\HttpFoundation\Request;
+use Ign\Bundle\ConfigurateurBundle\Controller\ModelController as ModelControllerBase;
+
+class ModelController extends ModelControllerBase {
+
+	/**
+	 * #600 : Forbid access to model creation.
+	 * User should only be able to
+	 * create a model with duplication functionality.
+	 * @Route("/models/new/", name="configurateur_model_new")
+	 */
+	public function newAction(Request $request) {
+		$this->addFlash('error', $this->get('translator')
+			->trans('datamodel.new.forbidden'));
+
+		return $this->redirectToRoute('configurateur_model_index');
+	}
+}
