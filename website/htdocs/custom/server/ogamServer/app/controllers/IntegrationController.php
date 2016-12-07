57cf60b23ab476d3be5a54c6e186ef65880040a8
Severine <severine.candelier@gmail.com>
2016-12-07 17:50:45 +0100

    #854: Download the list of  datasets in a csv

--- website/htdocs/custom/server/ogamServer/app/controllers/IntegrationController.php
+++ website/htdocs/custom/server/ogamServer/app/controllers/IntegrationController.php
@@ -47,6 +47,97 @@ class Custom_IntegrationController extends IntegrationController {
 		$this->render('custom-show-data-submission-page');
 	}
 
+    /**
+     * Download the list of submisssions/datasets as a csv file
+     */
+	public function downloadActiveSubmissionsCsvAction() {
+        $this->logger->debug('downloadActiveSubmissionsCsvAction');
+        // Configure memory and time limit because the program ask a lot of resources
+        $configuration = Zend_Registry::get("configuration");
+        ini_set("memory_limit", $configuration->getConfig('memory_limit', '1024M'));
+        ini_set("max_execution_time", 0);
+
+        // Get the current data submissions
+        $submissions = $this->submissionModel->getActiveSubmissions();
+
+        // Create the array to output to the csv file
+        $results = array();
+
+        $resultHeader = array(
+            "Soumission",
+            "Date",
+            "Organisme",
+            "Utilisateur",
+            "Modèle d'import",
+            "Fichier",
+            "Lignes",
+            "Etape d'import",
+            "Statut de l'import",
+            "Données publiées",
+        );
+
+        foreach ($submissions as $submission) {
+            $resultLine = array();
+
+            // Submission id
+            $resultLine[] = $submission->submissionId;
+            // Date
+            $resultLine[] = $submission->date;
+            // Provider
+            $resultLine[] = $submission->providerLabel;
+            // User (which has done the submission)
+            $resultLine[] = $submission->userLogin;
+            // Import model
+            $resultLine[] = $submission->datasetLabel;
+            // Files and lines
+            $files = array();
+            $lines = array();
+            foreach ($submission->files as $file) {
+                $files[] = basename($file->fileName);
+                $lines[] = $file->lineNumber;
+            }
+            // Files
+            $resultLine[] = implode(",",$files);
+            // Number of lines in the file
+            $resultLine[] = implode(",",$lines);
+            // Step of integration
+            $resultLine[] = $submission->step;
+            // Status of integration
+            $resultLine[] = $submission->status;
+            // Data validated ?
+            $resultLine[] = ($submission->step == "VALIDATE" && ($submission->status == "OK" || $submission->status == "WARNING")) ? "Oui" : "Non";
+
+            $results[] = $resultLine;
+        }
+
+        // -- Export results to a CSV file
+
+        $fileName = "Liste_JDD_" . preg_replace('/\s+/', '-', $configuration->getConfig('site_name', 'GINCO')) . "_" . date("d-m-Y") . ".csv";
+
+        // Define the header of the response
+        $this->getResponse()->setHeader('Content-Type', 'text/csv;charset=' . $configuration->getConfig('csvExportCharset', 'UTF-8') . ';application/force-download;', true);
+        $this->getResponse()->setHeader('Content-disposition', 'attachment; filename=' . $fileName, true);
+
+        // Prepend the Byte Order Mask to inform Excel that the file is in UTF-8
+        if ($configuration->getConfig('csvExportCharset', 'UTF-8') == 'UTF-8') {
+            echo(chr(0xEF));
+            echo(chr(0xBB));
+            echo(chr(0xBF));
+        }
+
+        // Opens the standard output as a file flux
+        $out = fopen('php://output', 'w');
+        fputcsv($out, $resultHeader, ';');
+
+        foreach ($results as $resultLine) {
+            fputcsv($out, $resultLine, ';');
+        }
+        fclose($out);
+
+        $this->_helper->layout()->disableLayout();
+        $this->_helper->viewRenderer->setNoRender();
+    }
+
 	/**
 	 * Show the upload data page.
 	 *
