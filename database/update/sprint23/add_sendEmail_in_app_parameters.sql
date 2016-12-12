edbd589bfa32e6b7830199e57c115b00b8e83fae
Severine <severine.candelier@gmail.com>
2016-12-12 15:10:23 +0100

    Add an email for user 'developpeur' Email = 'sinp-dev@ign.fr' ==> to avoid  lost emails

--- database/update/sprint23/add_sendEmail_in_app_parameters.sql
+++ database/update/sprint23/add_sendEmail_in_app_parameters.sql
@@ -1,2 +1,4 @@
 INSERT INTO website.application_parameters(name, value, description)
 VALUES ('sendEmail','1','Send emails for real ? 1/true, 0/false');
+
+UPDATE website.users SET email='sinp-dev@ign.fr' WHERE user_login='developpeur';
