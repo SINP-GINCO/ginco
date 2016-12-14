efb84d1850c64fe65ea2e3e594d434a107b7eb26
Gautam Pastakia <gautam.pastakia@ign.fr>
2016-12-14 11:45:19 +0100

    #969: Disable results bbox calc if too many results

---
+++ database/update/sprint24/add_results_bbox_compute_threshold_in_app_parameters.sql
@@ -0,0 +1,2 @@
+INSERT INTO website.application_parameters (name, value, description) VALUES 
+( 'results_bbox_compute_threshold', 1000, 'Results bounding box compute value threshold. Above this value no calculation is done.');
