beb49ca6e1fff51ba5c0e92986432327c47719f8
Gautam Pastakia <gautam.pastakia@ign.fr>
2016-12-15 14:38:54 +0100

    #968: Format some files

--- website/htdocs/custom/client/ogamDesktop/overrides/ogamDesktop/app/controller/map/Main.js
+++ website/htdocs/custom/client/ogamDesktop/overrides/ogamDesktop/app/controller/map/Main.js
@@ -1,44 +1,46 @@
 Ext.define('Ginco.controller.map.Main', {
-    override: 'OgamDesktop.controller.map.Main',
+	override : 'OgamDesktop.controller.map.Main',
 
-    config: {
-		refs: {
-            detailTab: 'grid-detail-panel'
+	config : {
+		refs : {
+			detailTab : 'grid-detail-panel'
 		}
 	},
-    
-    /**
-     * Update the map on request success
-     *
-     * Override: fire a custom Event 'resultsPrepared', listened by Grids.js,
-     * in order to have a sequential order in ajax requests :
-     *
-     * ajaxBuildRequest --> ajaxGetResultsBBox --> // ajaxGetResultsColumns --> ajaxGetResultsRows
-     *                                             // mapProxy (map images)
-     *
-     * @private
-     */
-    onRequestSuccess: function() {
-    	// Clear the detail grid (map bottom panel)
-    	this.getDetailTab().reinitializeDetailGrid();
-		
-        this.getMapmainwin().mask(this.requestLoadingMessage);
-        Ext.Ajax.request({
-            url : Ext.manifest.OgamDesktop.requestServiceUrl +'ajaxgetresultsbbox',
-            success : function(response, options) {
-                var result = Ext.decode(response.responseText);
-                if (result.success) {
-                    this.setResultsBbox(result.resultsbbox);
-                    this.updateRequestLayers();
-                    Ext.GlobalEvents.fireEvent('resultsPrepared');
-                } else {
-                    OgamDesktop.toast(result.errorMessage, this.getresultsbboxErrorTitle);
-                    this.getMapmainwin().unmask();
-                }
-            },
-            scope: this
-        });
-    },
+
+	/**
+	 * Update the map on request success
+	 * 
+	 * Override: fire a custom Event 'resultsPrepared', listened by Grids.js, in
+	 * order to have a sequential order in ajax requests :
+	 * 
+	 * ajaxBuildRequest --> ajaxGetResultsBBox --> // ajaxGetResultsColumns -->
+	 * ajaxGetResultsRows // mapProxy (map images)
+	 * 
+	 * @private
+	 */
+	onRequestSuccess : function() {
+		// Clear the detail grid (map bottom panel)
+		this.getDetailTab().reinitializeDetailGrid();
+
+		this.getMapmainwin().mask(this.requestLoadingMessage);
+		Ext.Ajax.request({
+			url : Ext.manifest.OgamDesktop.requestServiceUrl
+					+ 'ajaxgetresultsbbox',
+			success : function(response, options) {
+				var result = Ext.decode(response.responseText);
+				if (result.success) {
+					this.setResultsBbox(result.resultsbbox);
+					this.updateRequestLayers();
+					Ext.GlobalEvents.fireEvent('resultsPrepared');
+				} else {
+					OgamDesktop.toast(result.errorMessage,
+							this.getresultsbboxErrorTitle);
+					this.getMapmainwin().unmask();
+				}
+			},
+			scope : this
+		});
+	},
 
     /**
      * Show the map container and zoom on the result BBox.
