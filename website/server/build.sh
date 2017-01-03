9ab66a8e46440654b6322c36eebb5ff7542ba4d8
Severine <severine.candelier@gmail.com>
2017-01-03 17:53:02 +0100

    #984/985: Add build scripts

---
+++ website/server/build.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+set -e
+set -x
+#------------------------------------------------------------------------------
+# Build the project for prod environment
+# parameters are all forward to composer.
+#------------------------------------------------------------------------------
+
+# get composer
+curl -sS https://getcomposer.org/installer | php
+# get dependencies and build
+./composer.phar install $@
+
+# install assets
+php app/console assets:install
+
+# Dump Assetic assets for prod environnement, clear and warmup cache
+php app/console assetic:dump --env=prod
+php app/console cache:clear --env=prod
+php app/console cache:warmup --env=prod
+
