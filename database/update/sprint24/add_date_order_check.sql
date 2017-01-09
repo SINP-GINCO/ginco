4ad1f1d94f07dc0ad7b829300c00fe40ef64565d
amouget <anna.mouget@ign.fr>
2017-01-09 14:57:02 +0100

    Adapt database update scripts

--- database/update/sprint24/add_date_order_check.sql
+++ database/update/sprint24/add_date_order_check.sql
@@ -1,2 +1,5 @@
+DELETE FROM metadata.checks WHERE check_id=1209;
+DELETE FROM metadata_work.checks WHERE check_id=1209;
+
 INSERT INTO metadata.checks(check_id, step, name, label, description, statement, importance) VALUES (1209, 'CONFORMITY', 'DATE_ORDER', 'Incohérence sur la chronologie des dates.', 'Incohérence sur la chronologie des dates.', null, 'ERROR');
 INSERT INTO metadata_work.checks(check_id, step, name, label, description, statement, importance) VALUES (1209, 'CONFORMITY', 'DATE_ORDER', 'Incohérence sur la chronologie des dates.', 'Incohérence sur la chronologie des dates.', null, 'ERROR');
--- database/update/sprint24/add_results_bbox_compute_threshold_in_app_parameters.sql
+++ database/update/sprint24/add_results_bbox_compute_threshold_in_app_parameters.sql
@@ -1,2 +1,4 @@
+DELETE FROM website.application_parameters WHERE name='results_bbox_compute_threshold';
+
 INSERT INTO website.application_parameters (name, value, description) VALUES 
 ( 'results_bbox_compute_threshold', 1000, 'Results bounding box compute value threshold. Above this value no calculation is done.');
