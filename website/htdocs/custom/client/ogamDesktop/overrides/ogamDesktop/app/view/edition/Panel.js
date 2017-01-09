0825e9e4825e041ec5ff2199a21b64e151ce7ee3
amouget <anna.mouget@ign.fr>
2017-01-09 09:35:28 +0100

    #926: Add form structure in edition page and details panel

--- website/htdocs/custom/client/ogamDesktop/overrides/ogamDesktop/app/view/edition/Panel.js
+++ website/htdocs/custom/client/ogamDesktop/overrides/ogamDesktop/app/view/edition/Panel.js
@@ -6,15 +6,19 @@ Ext.define('Ginco.view.edition.Panel', {
     fieldWidth : 650,
 
     toggleVisibleButton : null,
+	/**
+	 * @cfg {Ext.form.FieldSet} a group of items.
+	 */
+	dataGroupFS : null,
 
     /**
      * Overwrite : add the "toggle visibility for optionel fields" button
      */
     initComponent: function () {
 
-        // Call ogam parent initComponent
-        this.callParent();
-
+		// Call ogam parent initComponent
+		this.callParent(arguments);
+        
         // Hide/Show not-required fields button
         this.toggleVisibleButton = new Ext.Button({
             text: this.toggleVisibilityLabel,
@@ -38,19 +42,93 @@ Ext.define('Ginco.view.edition.Panel', {
       */
     toggleOptionnalFieldsVisibility: function () {
         Ext.each(Ext.select('.not-required'), function (item) {
-            // console.log(item);
             item.toggle();
         });
         // Redo the layout to adjust height
         this.dataEditForm.updateLayout();
     },
 
+    /**
+     * Overwrite: Add form groups
+	 * Add the form items to the field form.
+	 * @private
+	 * @param {Ext.data.Record} records The records
+	 */
+	buildFieldForm : function(store, records) {
+		var dataProvider = '';
+		
+		// Sorts store fields according to form_format.position
+		store.sort('position');
+		// Easier to take records from the store : it is sorted
+		records = store.data.items;
+		
+		// Splits items in sub-forms according to the position of metadata.form_format they belongs to
+		var formItems = [];
+		
+		for ( var i = 0; i < records.length; i++) {
+          	var formItem = formItems[records[i].data.formPosition];
+        	if (!formItem) {
+        		// There is not already a form with this position : we create it
+        		formItems[records[i].data.formPosition] = [];
+        		formItem = formItems[records[i].data.formPosition];
+        	}
+			var item = this.getFieldConfig(records[i].data, true);
+			formItem.push(item);
+
+			if (item.name.indexOf('PROVIDER_ID') !== -1) { // detect the provider id
+				dataProvider = item.value;
+			}
+		}
+
+		for (var i = 1; i < formItems.length; i++) {
+			// Do not display an empty form
+			if (formItems[i]) {
+				if (formItems[i][0].formLabel != "Autres" || formItems[i].length > 3) {
+					// Add the form to the form of the form Panel
+					this.dataGroupFS = new Ext.form.FieldSet({
+						title : "Catégorie : " + formItems[i][0].formLabel,
+						items : formItems[i],
+						width: '100%'
+					});
+					this.dataEditForm.insert(this.dataGroupFS);
+				}
+			}
+
+		}
+		
+		// Add a hidden field for the mode (ADD or EDIT)
+		modeItem = {
+			xtype : 'hidden',
+			name : 'MODE',
+			hiddenName : 'MODE',
+			value : this.mode
+		};
+		this.dataEditForm.add(modeItem);
+
+		// Check the rights on the data for the validate button
+		if (this.checkEditionRights) {
+
+			// Look for the provider of the data
+			if (OgamDesktop.userProviderId !== dataProvider) {
+				this.validateButton.disable();
+				this.validateButton.setTooltip(this.dataEditFSValidateButtonDisabledTooltip);
+			}
+		}
+
+		this.unmask();
+
+		// Redo the layout
+		this.dataEditForm.updateLayout();
+	},    
 
     /**
      * Overwrite: dont'show at all non editable/inseratble fields
+     * Overwrite: Add formLabel to display form groups
      */
     getFieldConfig : function(record) {
         var field = this.callParent(arguments);
+        
+        field.formLabel = record.formLabel;
 
         if ((this.mode === this.editMode && !Ext.isEmpty(record.editable) && record.editable !== "1")
             || (this.mode === this.addMode && !Ext.isEmpty(record.insertable) && record.insertable !== "1")) {
--- website/htdocs/custom/client/ogamDesktop/overrides/ogamDesktop/app/view/navigation/Tab.js
+++ website/htdocs/custom/client/ogamDesktop/overrides/ogamDesktop/app/view/navigation/Tab.js
@@ -13,34 +13,30 @@ Ext.define('Ginco.view.navigation.Tab', {
         this.tpl = new Ext.XTemplate(
             '<div>', // Required by the print function
                 '<h2 class="o-navigation-title">{title}</h2>',
-//                '<div class="o-navigation-map-table">',
-//                    '<div class="o-navigation-map-left-col">',
-//                        '<div class="o-navigation-map-img">',
-//                            '<tpl for="maps1.urls">',
-//                                '<img src="{url}">',
-//                            '</tpl>',
-//                        '</div>',
-//                    '</div>',
-//                '</div>',
 				'<tpl for="formats">',
 					'<div class="o-navigation-fieldset">',
 						'<div class="o-navigation-fieldset-title">',
                             '<tpl if="xindex &lt; xcount">',
                                 '<div class="o-navigation-fieldset-title-link" onclick="Ext.ComponentQuery.query(\'navigation-mainwin\')[0].openDetails(\'{editURL}\');"></div>',
                             '</tpl>',
-                            '<span>{title}</span>',
+                            '<span>Table : {title}</span>',
                         '</div>',
 							'<tpl for="fields">',
-						        '<tpl switch="inputType">',
-						            '<tpl case="CHECKBOX">',
-						            	'<span><b>{label} :</b> {[this.convertBoolean(values)]}</span>',
-						            '<tpl case="IMAGE">',
-										'{[(Ext.isEmpty(values.value) || (Ext.isString(values.value) && Ext.isEmpty(values.value.trim()))) ? \'\' : \'<img title=\"\' + values.label + \'\" src=\"' + window.location.origin + '/img/photos/\' + values.value + \'\">\']}',
-						            '<tpl default>',
-						            	'<tpl if="type ==\'STRING\' && subtype==\'LINK\' && value.length &gt; 0">',
-						            		'<span><b>{label} :</b> <a class="external" href="{value}" target="_blank"><span>' + this.linkFieldDefaultText + '</span></a></span>',
-						            	'<tpl else>',
-						            		'<span><b>{label} :</b> {[(Ext.isEmpty(values.value) || (Ext.isEmpty(values.value.toString().trim()))) ? "-" : values.value.toString()]}</span>',
+								'<div class="o-navigation-fieldset-title-form">',
+									'<span>Catégorie : {formulaire}</span>',
+								'</div>',
+								'<tpl for="champs">',
+						        	'<tpl switch="inputType">',
+						            	'<tpl case="CHECKBOX">',
+						            		'<span><b>{label} :</b> {[this.convertBoolean(values)]}</span>',
+						            	'<tpl case="IMAGE">',
+						            		'{[(Ext.isEmpty(values.value) || (Ext.isString(values.value) && Ext.isEmpty(values.value.trim()))) ? \'\' : \'<img title=\"\' + values.label + \'\" src=\"' + window.location.origin + '/img/photos/\' + values.value + \'\">\']}',
+						            	'<tpl default>',
+						            		'<tpl if="type ==\'STRING\' && subtype==\'LINK\' && value.length &gt; 0">',
+						            			'<span><b>{label} :</b> <a class="external" href="{value}" target="_blank"><span>' + this.linkFieldDefaultText + '</span></a></span>',
+						            		'<tpl else>',
+						            			'<span><b>{label} :</b> {[(Ext.isEmpty(values.value) || (Ext.isEmpty(values.value.toString().trim()))) ? "-" : values.value.toString()]}</span>',
+						            		'</tpl>',
 						            	'</tpl>',
 						        '</tpl>',
 							'</tpl>',
@@ -56,9 +52,6 @@ Ext.define('Ginco.view.navigation.Tab', {
     								'<div class="o-navigation-childfieldset-leftcolumn" data-qtip="{1}" onclick="Ext.ComponentQuery.query(\'navigation-mainwin\')[0].openDetails(\'{0}\');"></div>',// OGAM-614 - TODO: Throw an event
     								'<div class="o-navigation-childfieldset-rightcolumn">{1}</div>',
     							'</div>',
-    //							'<tpl if="type == \'IMAGE\'">',
-    //								'{[(Ext.isEmpty(values.value) || (Ext.isString(values.value) && Ext.isEmpty(values.value.trim()))) ? \'\' : \'<img title=\"\' + values.label + \'\" src=\"' + window.location.origin + '/img/photos/\' + values.value + \'\">\']}',
-    //							'</tpl>',
     						'</tpl>',
     					'</div>',
     				'</div>',
@@ -92,6 +85,61 @@ Ext.define('Ginco.view.navigation.Tab', {
             actionMethods: {create: 'POST', read: 'POST', update: 'POST', destroy: 'POST'},
             success :function(response, options){
                 var data = Ext.decode(response.responseText);
+                                
+                // Sorts Objects according to property
+                function dynamicSort(property) {
+                    return function (a,b) {
+                        var result = (a[property] < b[property]) ? -1 : (a[property] > b[property]) ? 1 : 0;
+                        return result;
+                    }
+                }
+                
+                // Sorts objects according to multiple properties
+                function dynamicSortMultiple() {
+                    /*
+                     * save the arguments object as it will be overwritten
+                     * note that arguments object is an array-like object
+                     * consisting of the names of the properties to sort by
+                     */
+                    var props = arguments;
+                    return function (obj1, obj2) {
+                        var i = 0, result = 0, numberOfProperties = props.length;
+                        /* try getting a different result from 0 (equal)
+                         * as long as we have extra properties to compare
+                         */
+                        while(result === 0 && i < numberOfProperties) {
+                            result = dynamicSort(props[i])(obj1, obj2);
+                            i++;
+                        }
+                        return result;
+                    }
+                }
+
+                // TODO : add foreach on data.format[i] ?
+                data.formats[0].fields.sort(dynamicSortMultiple("formPosition", "position"));   
+                
+                var formArrays = Object.create(null);
+
+            	// Loop the fields array
+                data.formats[0].fields.forEach(function(field) {
+                	// Get the form array for this field'formLabel, if any
+                	var formArray = formArrays[field.formLabel];
+                	if (!formArray) {
+                		// There wasn't one, create it
+                		formArray = formArrays[field.formLabel] = [];
+                	}
+                	// Add this entry
+                	formArray.push(field);
+            	});
+
+                var tab = [];
+                for (var key in formArrays) {                    
+                    tab.push(Object.create({'formulaire': key, 'champs' :formArrays[key]}));
+                    
+                }
+                
+                data.formats[0].fields = tab;
+                
                 var title = data.title;
                 if(data.title.length > this.titleCharsMaxLength){
                     title = data.title.substring(0,this.titleCharsMaxLength) + '...';
