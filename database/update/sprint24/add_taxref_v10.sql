4dcc37076392b0ef5b6285dd2cdb5da16f7a3518
amouget <anna.mouget@ign.fr>
2017-01-10 15:00:02 +0100

    #928: Update taxref to v10

--- database/update/sprint24/add_taxref_v10.sql
+++ database/update/sprint24/add_taxref_v10.sql
@@ -1,4 +1,4 @@
-\COPY referentiels.taxref FROM '$dataDir/taxref.txt' WITH NULL '', FORMAT 'csv', HEADER, DELIMITER E'\t', ENCODING 'UTF-8';
+\COPY referentiels.taxref FROM '@sprintDir@/taxref.txt' WITH (NULL '', FORMAT 'csv', HEADER, DELIMITER E'\t', ENCODING 'UTF-8');
 
 SET search_path TO public, metadata;
 SET client_encoding = 'UTF-8';
@@ -11,7 +11,7 @@ DELETE FROM metadata.mode_taxref;
 --We insert cd_nom and not lb_name in name to see code in TAXREF subtype fields (cdNom and cdRef).
 --SELECT 'TaxRefValue', cd_nom,  cd_taxsup, lb_nom, nom_complet, nom_vern, '0', case when (cd_nom = cd_ref) then 1 else 0 end
 INSERT INTO metadata.mode_taxref (unit, code, parent_code, "name", lb_name, complete_name, vernacular_name, is_leaf, is_reference)
-SELECT 'TaxRefValue', taxref.cd_nom, coalesce(taxref.cd_taxsup, trf.cd_taxsup), taxref.cd_nom, taxref.lb_nom, taxref.nom_complet, taxref.nom_vern, '0', case when (taxref.cd_nom = taxref.cd_ref) then 1 else 0 end
+SELECT 'TaxRefValue', taxref.cd_nom, coalesce(taxref.cd_taxsup, rtf.cd_taxsup), taxref.cd_nom, taxref.lb_nom, taxref.nom_complet, taxref.nom_vern, '0', case when (taxref.cd_nom = taxref.cd_ref) then 1 else 0 end
 FROM referentiels.taxref
 -- Fills cd_taxsup (only given when cd_nom = cd_ref)
 inner join referentiels.taxref rtf on (rtf.cd_nom = taxref.cd_ref);
