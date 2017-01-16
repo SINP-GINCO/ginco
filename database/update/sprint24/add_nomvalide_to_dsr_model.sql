e6253a948568c37b9a904ce84d401bbd49ab84ea
Gautam <gautam.pastakia@ign.fr>
2017-01-16 11:30:17 +0100

    #954: Remove nomvalide from file format

--- database/update/sprint24/add_nomvalide_to_dsr_model.sql
+++ database/update/sprint24/add_nomvalide_to_dsr_model.sql
@@ -30,8 +30,4 @@ INSERT INTO metadata_work.form_field VALUES ('nomvalide', 'form_observation', '1
 
 INSERT INTO metadata_work.table_field VALUES ('nomvalide', 'table_observation', 'nomvalide', '1', '1', '1', '0', 104, NULL);
 
-INSERT INTO metadata_work.file_field (data, format, is_mandatory, mask, "position") VALUES ('nomvalide','file_observation','0', NULL, 89);
-
-INSERT INTO metadata_work.field_mapping VALUES ('nomvalide', 'file_observation', 'nomvalide', 'table_observation', 'FILE');
-
 INSERT INTO metadata_work.dataset_fields VALUES ('dataset_2', 'RAW_DATA', 'table_observation', 'nomvalide');
