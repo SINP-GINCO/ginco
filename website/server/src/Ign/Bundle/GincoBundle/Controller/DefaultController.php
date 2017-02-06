4b473650940c051d011afaaf23cb6bfeb65bd8f9
scandel <severine.candelier@ign.fr>
2017-02-06 18:27:37 +0100

    #1008: Override Homepage twig template

--- website/server/src/Ign/Bundle/GincoBundle/Controller/DefaultController.php
+++ website/server/src/Ign/Bundle/GincoBundle/Controller/DefaultController.php
@@ -6,8 +6,5 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class DefaultController extends Controller
 {
-    public function indexAction()
-    {
-        return $this->render('IgnGincoBundle:Default:index.html.twig');
-    }
+
 }
