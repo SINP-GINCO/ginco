48069cb0d633e82edaf52427dc41804c216acb9c
amouget <anna.mouget@ign.fr>
2017-02-13 15:17:37 +0100

    #1066: Prepare sprint 26

---
+++ database/update/sprint26/update_db_sprint.php
@@ -0,0 +1,32 @@
+<?php
+$sprintDir = dirname(__FILE__);
+require_once "$sprintDir/../../../lib/share.php";
+
+// -------------------------------------------------------------------------------
+// Synopsis: migrate DB GINCO from sprint 25 to sprint 26
+// -------------------------------------------------------------------------------
+function usage($mess = NULL) {
+	echo "------------------------------------------------------------------------\n";
+	echo ("\nUpdate DB from sprint 25 to sprint 26\n");
+	echo ("> php update_db_sprint.php -f <configFile> [{-D<propertiesName>=<Value>}]\n\n");
+	echo "o <configFile>: a java style properties file for the instance on which you work\n";
+	echo "o -D : inline options to complete or override the config file.\n";
+	echo "------------------------------------------------------------------------\n";
+	if (!is_null($mess)) {
+		echo ("$mess\n\n");
+		exit(1);
+	}
+	exit();
+}
+
+if (count($argv) == 1)
+	usage();
+$config = loadPropertiesFromArgs();
+
+try {
+	/* patch code here */
+} catch (Exception $e) {
+	echo "$sprintDir/update_db_sprint.php\n";
+	echo "exception: " . $e->getMessage() . "\n";
+	exit(1);
+}
