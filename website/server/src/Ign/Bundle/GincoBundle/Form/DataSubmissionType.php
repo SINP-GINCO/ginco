d4594a4f616b3907530a60a652200e52c47ea0ab
Gautam <gautam.pastakia@ign.fr>
2017-02-13 15:33:21 +0100

    #1000: Migrate import pages custom Ginco dev
    Add provider in upload form. Fixed terminology specific to Ginco.Add term 'File' before FileFormat label in data upload form. Remove all code related to this commit in server_zend.

---
+++ website/server/src/Ign/Bundle/GincoBundle/Form/DataSubmissionType.php
@@ -0,0 +1,51 @@
+<?php
+namespace Ign\Bundle\GincoBundle\Form;
+
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Ign\Bundle\OGAMBundle\Form\DataSubmissionType as BaseType;
+
+class DataSubmissionType extends BaseType {
+
+	/**
+	 * Build the data submission form.
+	 *
+	 * @param FormBuilderInterface $builder
+	 * @param array $options
+	 */
+	public function buildForm(FormBuilderInterface $builder, array $options) {
+		dump($options['defaultProvider']);
+		$builder->add('DATASET_ID', ChoiceType::class, array(
+			'label' => 'Dataset',
+			'required' => true,
+			'choice_value' => 'id',
+			'choice_label' => 'label',
+			'choices' => $options['datasetChoices'],
+			'choices_as_values' => true
+		))
+			->add('PROVIDER_ID', ChoiceType::class, array(
+			'label' => 'Provider',
+			'required' => true,
+			'choice_value' => 'id',
+			'choice_label' => 'label',
+			'choices' => $options['providerChoices'],
+			'choices_as_values' => true,
+			'preferred_choices' => array($options['defaultProvider'])
+		))
+			->add('submit', SubmitType::class);
+	}
+
+	/**
+	 *
+	 * @param OptionsResolver $resolver
+	 */
+	public function configureOptions(OptionsResolver $resolver) {
+		$resolver->setDefaults(array(
+			'datasetChoices' => null,
+			'providerChoices' => null,
+			'defaultProvider' => null
+		));
+	}
+}
