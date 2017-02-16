92d4ed6448aff6a26623f084c72cc56e98f6405c
Gautam <gautam.pastakia@ign.fr>
2017-02-16 11:52:23 +0100

    #1037: Download referentiels only if not exist locally
    Added variables in script to add version in filename in order to be sure to have at least same version. Move downloaded files to download folder.

--- database/init/referentiels/init_referentiels.sh
+++ database/init/referentiels/init_referentiels.sh
@@ -24,30 +24,60 @@ connectionStr=$@
 rootDir=$(dirname $(readlink -f $0))
 dataDir=$rootDir/data
 
+taxref=$dataDir/download/taxrefv10.txt
+communes=$dataDir/download/visu_communes_geofla_2015.1.sql
+departements=$dataDir/download/visu_departements_geofla_2015.1.sql
+regions=$dataDir/download/visu_regions_geofla_2015.1.sql
 
 # Note NV: Il aurait été sympa de pouvoir récupérer n'importe quelle version de
 #       taxref et de faire passer les mêmes traitements. Mais finalement, je renonce
 #       parce que chaque version de taxref est un peu différente.
-echo "téléchargement de taxref v10..."
-wget "https://ginco.ign.fr/ref/TAXREFv10.0/TAXREFv10.0.txt" -O $dataDir/taxref.txt
+if [ -f "$taxref" ]
+then
+	echo "$taxref a été trouvé localement..."
+else
+	echo "téléchargement de taxref v10..."
+	wget "https://ginco.ign.fr/ref/TAXREFv10.0/TAXREFv10.0.txt" -O $taxref
+fi
 echo "Intégration des données taxref dans la base..."
 psql "$connectionStr" -f $rootDir/create_taxref10_tables.sql
 copyOptions="NULL '', FORMAT 'csv', HEADER, DELIMITER E'\t', ENCODING 'UTF-8'"
-psql "$connectionStr" -c "\COPY referentiels.taxref FROM '$dataDir/taxref.txt' WITH ($copyOptions);"
+psql "$connectionStr" -c "\COPY referentiels.taxref FROM '$taxref' WITH ($copyOptions);"
 echo "Intégration de TAXREF terminée."
 
 echo "Intégration du référentiel de sensiblité"
 #FIXME: ajouter le téléchargement du référentiel de sensibilité compatible avec le taxref 10!
 psql "$connectionStr" -f $dataDir/especesensible.sql
 
-echo "téléchargement de la dernière version des limites administratives (geoFLA)..."
-wget "https://ginco.ign.fr/ref/geofla_last_communes.sql"     -O $dataDir/visu_communes.sql
-wget "https://ginco.ign.fr/ref/geofla_last_departements.sql" -O $dataDir/visu_departements.sql
-wget "https://ginco.ign.fr/ref/geofla_last_regions.sql"      -O $dataDir/visu_regions.sql
+if [ -f "$communes" ]
+then
+	echo "$communes a été trouvé localement..."
+else
+	echo "téléchargement de la dernière version des limites administratives (geoFLA communes)..."
+	wget "https://ginco.ign.fr/ref/geofla_last_communes.sql" -O $communes
+
+fi
+
+if [ -f "$departements" ]
+then
+	echo "$departements a été trouvé localement..."
+else
+	echo "téléchargement de la dernière version des limites administratives (geoFLA départements)..."
+	wget "https://ginco.ign.fr/ref/geofla_last_departements.sql" -O $departements
+fi
+
+if [ -f "$regions" ]
+then
+	echo "$regions a été trouvé localement..."
+else
+	echo "téléchargement de la dernière version des limites administratives (geoFLA régions)..."
+	wget "https://ginco.ign.fr/ref/geofla_last_regions.sql"      -O $regions
+fi
+
 echo "Intégration des limites administratives pour la visu..."
-psql "$connectionStr" -f $dataDir/visu_communes.sql
-psql "$connectionStr" -f $dataDir/visu_departements.sql
-psql "$connectionStr" -f $dataDir/visu_regions.sql
+psql "$connectionStr" -f "$communes"
+psql "$connectionStr" -f "$departements"
+psql "$connectionStr" -f "$regions"
 echo "Mise à jour des référentiels des limites administratives terminée."
 
 echo "Création des autres référentiels métier"
