920d4c84d78cf009c4d940a82cb070519d3852a0
scandel <severine.candelier@ign.fr>
2017-02-24 15:39:16 +0100

    #1074: Update postgis to 2.3.1

--- database/update/sprint26/update_db_sprint.php
+++ database/update/sprint26/update_db_sprint.php
@@ -25,7 +25,7 @@ $config = loadPropertiesFromArgs();
 
 try {
 	/* patch code here */
-	//execCustSQLFile("$sprintDir/add_sensirefversion.sql", $config);
+	execCustSQLFile("$sprintDir/update_postgis_to_2.3.1.sql", $config);
 } catch (Exception $e) {
 	echo "$sprintDir/update_db_sprint.php\n";
 	echo "exception: " . $e->getMessage() . "\n";
