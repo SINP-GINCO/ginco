145f0a1b2a4e3ff58c9229d283ecaa8896fbb5a1
scandelier <severine.candelier@ign.fr>
2017-03-15 15:52:12 +0100

    Create sprint27 directory for db updates
    (cherry picked from commit 3546ae2)

---
+++ database/update/sprint27/update_db_sprint.php
@@ -0,0 +1,45 @@
+<?php
+$sprintDir = dirname(__FILE__);
+require_once "$sprintDir/../../../lib/share.php";
+
+// -------------------------------------------------------------------------------
+// Synopsis: migrate DB GINCO from sprint 26 to sprint 27
+// -------------------------------------------------------------------------------
+function usage($mess = NULL) {
+	echo "------------------------------------------------------------------------\n";
+	echo ("\nUpdate DB from sprint 26 to sprint 27\n");
+	echo ("> php update_db_sprint.php -f <configFile> [{-D<propertiesName>=<Value>}]\n\n");
+	echo "o <configFile>: a java style properties file for the instance on which you work\n";
+	echo "o -D : inline options to complete or override the config file.\n";
+	echo "------------------------------------------------------------------------\n";
+	if (!is_null($mess)) {
+		echo ("$mess\n\n");
+		exit(1);
+	}
+	exit();
+}
+
+if (count($argv) == 1)
+	usage();
+$config = loadPropertiesFromArgs();
+
+try {
+	/* patch code here */
+	// execCustSQLFile("$sprintDir/script.sql", $config);
+} catch (Exception $e) {
+	echo "$sprintDir/update_db_sprint.php\n";
+	echo "exception: " . $e->getMessage() . "\n";
+	exit(1);
+}
+
+$CLIParams = implode(' ', array_slice($argv, 1));
+/* patch user raw_data here */
+/* system("php $sprintDir/script.php $CLIParams", $returnCode1);
+ system("php $sprintDir/script2.php $CLIParams", $returnCode2);
+
+if ($returnCode1 != 0 || $returnCode2 != 0) {
+	echo "$sprintDir/update_db_sprint.php\n";
+	echo "exception: " . $e->getMessage() . "\n";
+	exit(1);
+}
+*/
