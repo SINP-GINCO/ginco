0cf81149c87c469bbb03e03004dd5e45cabae515
amouget <anna.mouget@ign.fr>
2017-05-16 17:57:33 +0200

    Fix database update script, add email when null

--- database/update/sprint28/update_users_table.sql
+++ database/update/sprint28/update_users_table.sql
@@ -1,6 +1,8 @@
 ALTER TABLE website.users ADD column activation_code VARCHAR(50);
 COMMENT ON COLUMN USERS.ACTIVATION_CODE IS 'The activation code for password reset';
 
+UPDATE website.users SET email='sinp-dev@ign.fr' WHERE email is null;
+
 ALTER TABLE website.users ALTER column user_login SET NOT NULL;
 ALTER TABLE website.users ALTER column email SET NOT NULL;
 
