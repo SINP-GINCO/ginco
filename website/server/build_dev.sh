7c54deaf8558084cf8f1f3ebf50b0c873cffbcd5
scandel <severine.candelier@ign.fr>
2017-05-30 18:34:34 +0200

    #1042: Put all application log files in website/server/app/logs directory

--- website/server/build_dev.sh
+++ website/server/build_dev.sh
@@ -10,6 +10,12 @@ HTTPDUSER=`ps axo user,comm | grep -E '[a]pache|[h]ttpd|[_]www|[w]ww-data|[n]gin
 sudo setfacl -R -m u:"$HTTPDUSER":rwX -m u:`whoami`:rwX app/cache app/logs app/sessions
 sudo setfacl -dR -m u:"$HTTPDUSER":rwX -m u:`whoami`:rwX app/cache app/logs app/sessions
 
+# Set permissions on logs to tomcat user
+TOMCATUSER=tomcat7
+
+sudo setfacl -R -m u:"$TOMCATUSER":rwX app/logs
+sudo setfacl -dR -m u:"$TOMCATUSER":rwX app/logs
+
 # get composer
 curl -sS https://getcomposer.org/installer | php
 # get dependencies and build
