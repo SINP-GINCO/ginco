a8d9f73f7d6f0dc0f4a56431c269ea7c7dfdbae8
amouget <anna.mouget@ign.fr>
2017-06-08 17:48:38 +0200

    #1137: Repare corrupted metadta.ods, dateDetermination Date type

--- database/update/sprint29/update_db_sprint.php
+++ database/update/sprint29/update_db_sprint.php
@@ -40,12 +40,11 @@ try {
 
 $CLIParams = implode(' ', array_slice($argv, 1));
 /* patch user raw_data here */
-//  system("php $sprintDir/script1.php $CLIParams", $returnCode1);
+  system("php $sprintDir/change_datedetermination_type_to_date.php $CLIParams", $returnCode1);
 // system("php $sprintDir/script2.php $CLIParams", $returnCode2);
-/*
-if ($returnCode1 != 0 || $returnCode2 != 0) {
+
+if ($returnCode1 != 0) {
 	echo "$sprintDir/update_db_sprint.php\n";
 	echo "exception: " . $e->getMessage() . "\n";
 	exit(1);
 }
-*/
