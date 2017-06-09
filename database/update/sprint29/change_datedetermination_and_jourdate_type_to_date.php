355e1f95677bf2534b4259f3bd47ec36b908ea9d
amouget <anna.mouget@ign.fr>
2017-06-09 10:37:21 +0200

    #1137: Fix date format

--- database/update/sprint29/change_datedetermination_and_jourdate_type_to_date.php
+++ database/update/sprint29/change_datedetermination_and_jourdate_type_to_date.php
@@ -9,25 +9,35 @@ try {
 	$dbconn = pg_connect($conn_string) or die('Connection failed');
 	
 	/* Change dateDetermination type from dateTime to date, in metadata and metadata_work, and update the data in raw_data. */
-	$columnsToChange = ['datedetermination'];
+	$columnsToChange = ['jourdatedebut', 'jourdatefin', 'datedetermination'];
 	
 	foreach ($columnsToChange as $columnToChange) {
-		
 		$updateMetadataQuery = "
 			UPDATE metadata_work.data SET unit='Date' WHERE data ='$columnToChange';
 			UPDATE metadata.data SET unit='Date' WHERE data = '$columnToChange';"
 		;
 		pg_query($updateMetadataQuery);
 		
-		$tablesToUpdateQuery = "SELECT table_name FROM information_schema.columns WHERE table_schema = 'raw_data' AND table_name LIKE 'model%' AND column_name = '$columnToChange' AND data_type= 'timestamp with time zone'";
+		$tablesToUpdateQuery = "SELECT table_name FROM information_schema.columns WHERE table_schema = 'raw_data' AND table_name LIKE 'model%' AND column_name = '$columnToChange'";
 		$tablesToUpdate = pg_query($tablesToUpdateQuery);
-		
+
 		while ($table = pg_fetch_assoc($tablesToUpdate)) {
 			echo "Traitement sur la table : " . $table['table_name'] . "\n";
 			
-			$alterTable = "ALTER TABLE " . $table['table_name'] . " ALTER COLUMN $columnToChange SET DATA TYPE Date";
+			// Drop trigger to change jourdatefin type (as we cannot modify the type of a column if used in a trigger)
+			$disableTrigger = "DROP TRIGGER sensitive_automatic" . $table['table_name'] . " ON raw_data." . $table['table_name'] . ";";
+			pg_query($disableTrigger);
 			
+			$alterTable = "ALTER TABLE " . $table['table_name'] . " ALTER COLUMN $columnToChange SET DATA TYPE Date";
 			pg_query($alterTable);
+			
+			// Recreate trigger
+			$enableTrigger = "CREATE TRIGGER sensitive_automatic" . $table['table_name'] . "
+								BEFORE UPDATE OF cdref, cdnom, jourdatefin, occstatutbiologique, codedepartementcalcule
+								ON raw_data." . $table['table_name'] . "
+								FOR EACH ROW
+								EXECUTE PROCEDURE raw_data.sensitive_automatic();";
+			pg_query($enableTrigger);
 		}
 	}
 	pg_close($dbconn);
