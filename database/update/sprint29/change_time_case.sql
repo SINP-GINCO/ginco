86c60361553a25bb48e71402aae23e97bf402943
amouget <anna.mouget@ign.fr>
2017-06-09 11:24:03 +0200

    #1137: datedetermination mask is yyyy-MM-dd

--- database/update/sprint29/change_time_case.sql
+++ database/update/sprint29/change_time_case.sql
@@ -2,3 +2,8 @@ UPDATE metadata_work.data SET unit='Time' WHERE unit ='TIME';
 UPDATE metadata.data SET unit='Time' WHERE unit ='TIME';
 UPDATE metadata_work.unit SET unit='Time' WHERE unit ='TIME';
 UPDATE metadata.unit SET unit='Time' WHERE unit ='TIME';
+
+UPDATE metadata_work.form_field SET mask='yyyy-MM-dd' WHERE data='datedetermination';
+UPDATE metadata.form_field SET mask='yyyy-MM-dd' WHERE data='datedetermination';
+UPDATE metadata_work.file_field SET mask='yyyy-MM-dd' WHERE data='datedetermination';
+UPDATE metadata.file_field SET mask='yyyy-MM-dd' WHERE data='datedetermination';
