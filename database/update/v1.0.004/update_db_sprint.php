dabf24e885fd7d60efa4ae8082a9d7bc7cbdaaea
scandelier <severine.candelier@ign.fr>
2017-06-26 13:31:32 +0200

    Move updates to update directory for v1.0.4

--- database/update/v1.0.004/update_db_sprint.php
+++ database/update/v1.0.004/update_db_sprint.php
@@ -3,11 +3,11 @@ $sprintDir = dirname(__FILE__);
 require_once "$sprintDir/../../../lib/share.php";
 
 // -----------------------------------------------------------------------------------------
-// Synopsis: migrate DB GINCO from sprint 27 to v1.0.0 (Nothing to do because it's the same)
+// Synopsis: migrate DB GINCO from sprint v1.0.3 to v1.0.4
 // -----------------------------------------------------------------------------------------
 function usage($mess = NULL) {
 	echo "------------------------------------------------------------------------\n";
-	echo ("\nUpdate DB from sprint 27 to v1.0.0\n");
+	echo ("\nUpdate DB from v1.0.3 to v1.0.4\n");
 	echo ("> php update_db_sprint.php -f <configFile> [{-D<propertiesName>=<Value>}]\n\n");
 	echo "o <configFile>: a java style properties file for the instance on which you work\n";
 	echo "o -D : inline options to complete or override the config file.\n";
@@ -25,9 +25,21 @@ $config = loadPropertiesFromArgs();
 
 try {
 	/* patch code here*/
+	 execCustSQLFile("$sprintDir/add_cvdl_sensitive_liste.sql", $config);
 } catch (Exception $e) {
 	echo "$sprintDir/update_db_sprint.php\n";
 	echo "exception: " . $e->getMessage() . "\n";
 	exit(1);
 }
 
+
+$CLIParams = implode(' ', array_slice($argv, 1));
+/* patch user raw_data here */
+  system("php $sprintDir/change_jourdebut_jourfin_type_to_date.php $CLIParams", $returnCode1);
+// system("php $sprintDir/script2.php $CLIParams", $returnCode2);
+
+if ($returnCode1 != 0) {
+	echo "$sprintDir/update_db_sprint.php\n";
+	echo "exception: " . $e->getMessage() . "\n";
+	exit(1);
+}
