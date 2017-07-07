72343a563693f63c64e7a204679ef2641a50c821
scandel <severine.candelier@ign.fr>
2017-07-07 16:09:31 +0200

    #1002: Add DEE Entity, Repository and table

--- database/init/1-4-Create_raw_data_schema.sql
+++ database/init/1-4-Create_raw_data_schema.sql
@@ -366,33 +366,83 @@ ALTER FUNCTION raw_data.init_trigger()
   OWNER TO admin;
 
 
--------------------------------------------------------------------------------
--- Table: raw_data.export_file
--------------------------------------------------------------------------------
+/*==============================================================*/
+/* Table and Sequence : DEE																			*/
+/*==============================================================*/
 
--- DROP TABLE raw_data.export_file;
+-- Sequence: raw_data.dee_id_seq
 
-CREATE TABLE raw_data.export_file
+-- DROP SEQUENCE raw_data.dee_id_seq;
+
+CREATE SEQUENCE raw_data.dee_id_seq
+INCREMENT 1
+MINVALUE 1
+MAXVALUE 9223372036854775807
+START 1
+CACHE 1;
+ALTER TABLE raw_data.dee_id_seq
+	OWNER TO admin;
+GRANT ALL ON SEQUENCE raw_data.dee_id_seq TO admin;
+GRANT ALL ON SEQUENCE raw_data.dee_id_seq TO ogam;
+
+-- Table: raw_data.dee
+
+-- DROP TABLE raw_data.dee;
+
+CREATE TABLE raw_data.dee
 (
-	submission_id integer NOT NULL,
-	user_login character varying(50),
-	file_name character varying(500) NOT NULL,
-	created_at timestamp without time zone DEFAULT now(),
-	CONSTRAINT pk_submission_id PRIMARY KEY (submission_id),
-	CONSTRAINT fk_job_id FOREIGN KEY (job_id)
-	REFERENCES website.job_queue (id) MATCH SIMPLE
-	ON UPDATE CASCADE ON DELETE CASCADE,
-	CONSTRAINT fk_submission_id FOREIGN KEY (submission_id)
-	REFERENCES raw_data.submission (submission_id) MATCH SIMPLE
-	ON UPDATE CASCADE ON DELETE CASCADE,
-	CONSTRAINT fk_user_login FOREIGN KEY (user_login)
+	id integer NOT NULL,
+	jdd_id integer,
+	user_login character varying(50) DEFAULT NULL::character varying,
+	message_id integer,
+	filepath character varying(500) DEFAULT NULL::character varying,
+	status character varying(20) NOT NULL,
+	version integer NOT NULL,
+	comment text,
+	submissions text NOT NULL,
+	createdat timestamp(0) without time zone NOT NULL,
+	CONSTRAINT dee_pkey PRIMARY KEY (id),
+	CONSTRAINT fk_fc60c94448ca3048 FOREIGN KEY (user_login)
 	REFERENCES website.users (user_login) MATCH SIMPLE
-	ON UPDATE CASCADE ON DELETE SET NULL
+	ON UPDATE NO ACTION ON DELETE NO ACTION,
+	CONSTRAINT fk_fc60c944537a1329 FOREIGN KEY (message_id)
+	REFERENCES website.messages (id) MATCH SIMPLE
+	ON UPDATE NO ACTION ON DELETE NO ACTION,
+	CONSTRAINT fk_fc60c944595b2064 FOREIGN KEY (jdd_id)
+	REFERENCES raw_data.jdd (id) MATCH SIMPLE
+	ON UPDATE NO ACTION ON DELETE NO ACTION
 )
 WITH (
 OIDS=FALSE
 );
-ALTER TABLE raw_data.export_file
+ALTER TABLE raw_data.dee
 	OWNER TO admin;
-GRANT ALL ON TABLE raw_data.export_file TO admin;
-GRANT ALL ON TABLE raw_data.export_file TO ogam;
+GRANT ALL ON TABLE raw_data.dee TO admin;
+GRANT ALL ON TABLE raw_data.dee TO ogam;
+
+-- Index: raw_data.idx_fc60c94448ca3048
+
+-- DROP INDEX raw_data.idx_fc60c94448ca3048;
+
+CREATE INDEX idx_fc60c94448ca3048
+	ON raw_data.dee
+	USING btree
+	(user_login COLLATE pg_catalog."default");
+
+-- Index: raw_data.idx_fc60c944595b2064
+
+-- DROP INDEX raw_data.idx_fc60c944595b2064;
+
+CREATE INDEX idx_fc60c944595b2064
+	ON raw_data.dee
+	USING btree
+	(jdd_id);
+
+-- Index: raw_data.uniq_fc60c944537a1329
+
+-- DROP INDEX raw_data.uniq_fc60c944537a1329;
+
+CREATE UNIQUE INDEX uniq_fc60c944537a1329
+	ON raw_data.dee
+	USING btree
+	(message_id);
--- database/update/sprint30/update_db_sprint.php
+++ database/update/sprint30/update_db_sprint.php
@@ -30,6 +30,8 @@ try {
 	execCustSQLFile("$sprintDir/add_event_listener_jdd_service.sql", $config);
 	execCustSQLFile("$sprintDir/update_bbox.sql", $config);
 	execCustSQLFile("$sprintDir/update_sensitive_species.sql", $config);
+	execCustSQLFile("$sprintDir/add_messages_table.sql", $config);
+	execCustSQLFile("$sprintDir/add_dee_table.sql", $config);
 } catch (Exception $e) {
 	echo "$sprintDir/update_db_sprint.php\n";
 	echo "exception: " . $e->getMessage() . "\n";
