d236ae93f3f2325833033d37c00e22b07478203d
amouget <anna.mouget@ign.fr>
2017-07-17 11:03:02 +0200

    Fix en WFS layers after naming update

---
+++ database/update/v1.0.005/update_apb_and_pnr_en_layer_names.sql
@@ -0,0 +1,2 @@
+UPDATE mapping.layer SET service_layer_name='PROTECTEDAREAS.PNR:pnr' WHERE layer_name='PROTECTEDAREAS_PNR_WFS';
+UPDATE mapping.layer SET service_layer_name='PROTECTEDAREAS.APB:apb' WHERE layer_name='PROTECTEDAREAS_APB_WFS';
