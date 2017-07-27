01c605e6754899f2e002b0bda5600348f00759ca
scandel <severine.candelier@ign.fr>
2017-07-27 15:11:11 +0200

    #1172: migration script from zend v1.0.5 to symfony v2.0.0

---
+++ database/update/migrate_1.0.5_to_v2.0.0/migrate_jdds_and_dee.sql
@@ -0,0 +1,179 @@
+-- Create jdd_field, and migrate title and metadata_ids (datas)
+
+-- Table: raw_data.jdd_field
+
+-- DROP TABLE raw_data.jdd_field;
+
+CREATE TABLE raw_data.jdd_field
+(
+  jdd_id integer NOT NULL, -- foreign key to the jdd
+  key character varying(255) NOT NULL, -- key for the field
+  type character varying(20) NOT NULL, -- type of the data stored
+  value_string character varying(255), -- value if of type string
+  value_text text, -- value if of type text
+  value_integer integer, -- value if of type integer
+  value_float double precision, -- value if of type float
+  CONSTRAINT pk_jdd_field_id PRIMARY KEY (jdd_id, key),
+  CONSTRAINT fk_jdd_id FOREIGN KEY (jdd_id)
+  REFERENCES raw_data.jdd (id) MATCH SIMPLE
+  ON UPDATE CASCADE ON DELETE CASCADE
+)
+WITH (
+OIDS=FALSE
+);
+ALTER TABLE raw_data.jdd_field
+  OWNER TO admin;
+GRANT ALL ON TABLE raw_data.jdd_field TO admin;
+GRANT ALL ON TABLE raw_data.jdd_field TO ogam;
+COMMENT ON COLUMN raw_data.jdd_field.jdd_id IS 'foreign key to the jdd';
+COMMENT ON COLUMN raw_data.jdd_field.key IS 'key for the field';
+COMMENT ON COLUMN raw_data.jdd_field.type IS 'type of the data stored';
+COMMENT ON COLUMN raw_data.jdd_field.value_string IS 'value if of type string';
+COMMENT ON COLUMN raw_data.jdd_field.value_text IS 'value if of type text';
+COMMENT ON COLUMN raw_data.jdd_field.value_integer IS 'value if of type integer';
+COMMENT ON COLUMN raw_data.jdd_field.value_float IS 'value if of type float';
+
+-- migrate metadata_Ids and titles
+
+INSERT INTO jdd_field (jdd_id, key, type, value_string)
+SELECT id, 'metadataId', 'string', jdd_metadata_id
+FROM jdd;
+
+INSERT INTO jdd_field (jdd_id, key, type, value_string)
+SELECT id, 'title', 'string', title
+FROM jdd;
+
+-- Add jdd_id to submission and fill it
+
+ALTER TABLE submission ADD COLUMN jdd_id integer;
+ALTER TABLE submission ADD CONSTRAINT fk_jdd_id FOREIGN KEY (jdd_id)
+REFERENCES raw_data.jdd (id) MATCH SIMPLE
+ON UPDATE CASCADE ON DELETE RESTRICT;
+
+COMMENT ON COLUMN raw_data.submission.jdd_id IS 'Reference of the jdd of the submission';
+
+UPDATE submission
+SET jdd_id = jdd.id
+FROM jdd
+WHERE jdd.submission_id = submission.submission_id;
+
+-- Delete orphan submissions
+DELETE FROM raw_data.submission
+WHERE jdd_id IS NULL;
+
+
+-- Add provider and user columns to jdd and fill them
+ALTER TABLE  jdd ADD COLUMN provider_id character varying(36);
+ALTER TABLE  jdd ADD COLUMN user_login character varying(50);
+ALTER TABLE  jdd ADD CONSTRAINT fk_provider_id FOREIGN KEY (provider_id)
+REFERENCES website.providers (id) MATCH SIMPLE
+ON UPDATE CASCADE ON DELETE SET NULL;
+ALTER TABLE  jdd ADD CONSTRAINT fk_user_login FOREIGN KEY (user_login)
+REFERENCES website.users (user_login) MATCH SIMPLE
+ON UPDATE CASCADE ON DELETE SET NULL;
+
+UPDATE jdd
+SET provider_id = submission.provider_id,
+  user_login = submission.user_login
+FROM submission
+WHERE jdd.submission_id = submission.submission_id;
+
+-- Table export_file --> dee, and complete data
+
+-- Table: raw_data.dee
+
+-- DROP TABLE raw_data.dee;
+
+CREATE TABLE raw_data.dee
+(
+  id integer NOT NULL,
+  jdd_id integer,
+  user_login character varying(50) DEFAULT NULL::character varying,
+  message_id integer,
+  filepath character varying(500) DEFAULT NULL::character varying,
+  status character varying(20) NOT NULL,
+  version integer NOT NULL,
+  comment text,
+  submissions text,
+  createdat timestamp(0) without time zone NOT NULL,
+  CONSTRAINT dee_pkey PRIMARY KEY (id),
+  CONSTRAINT fk_fc60c94448ca3048 FOREIGN KEY (user_login)
+  REFERENCES website.users (user_login) MATCH SIMPLE
+  ON UPDATE NO ACTION ON DELETE NO ACTION,
+  CONSTRAINT fk_fc60c944537a1329 FOREIGN KEY (message_id)
+  REFERENCES website.messages (id) MATCH SIMPLE
+  ON UPDATE NO ACTION ON DELETE NO ACTION,
+  CONSTRAINT fk_fc60c944595b2064 FOREIGN KEY (jdd_id)
+  REFERENCES raw_data.jdd (id) MATCH SIMPLE
+  ON UPDATE NO ACTION ON DELETE NO ACTION
+)
+WITH (
+OIDS=FALSE
+);
+ALTER TABLE raw_data.dee
+  OWNER TO admin;
+GRANT ALL ON TABLE raw_data.dee TO admin;
+GRANT ALL ON TABLE raw_data.dee TO ogam;
+
+-- Index: raw_data.idx_fc60c94448ca3048
+
+-- DROP INDEX raw_data.idx_fc60c94448ca3048;
+
+CREATE INDEX idx_fc60c94448ca3048
+  ON raw_data.dee
+  USING btree
+  (user_login COLLATE pg_catalog."default");
+
+-- Index: raw_data.idx_fc60c944595b2064
+
+-- DROP INDEX raw_data.idx_fc60c944595b2064;
+
+CREATE INDEX idx_fc60c944595b2064
+  ON raw_data.dee
+  USING btree
+  (jdd_id);
+
+-- Index: raw_data.uniq_fc60c944537a1329
+
+-- DROP INDEX raw_data.uniq_fc60c944537a1329;
+
+CREATE UNIQUE INDEX uniq_fc60c944537a1329
+  ON raw_data.dee
+  USING btree
+  (message_id);
+
+-- migrate datas from export_file
+
+
+INSERT INTO dee (id, user_login, filepath, status, version, createdat)
+  SELECT id, user_login, file_name, 'OK', 1, created_at
+  FROM export_file;
+
+UPDATE dee
+SET jdd_id = jdd.id,
+  submissions = jdd.submission_id
+FROM jdd
+WHERE jdd.export_file_id = dee.id;
+
+-- New filepaths
+UPDATE dee
+SET filepath = '/dee/' || replace(regexp_replace(filepath, '^.+[/\\]', ''), 'xml', 'zip');
+
+-- Drop old table export_file
+DROP TABLE IF EXISTS raw_data.export_file CASCADE;
+
+-- Clean table jdd
+ALTER TABLE jdd DROP COLUMN jdd_metadata_id;
+ALTER TABLE jdd DROP COLUMN title;
+ALTER TABLE jdd DROP COLUMN submission_id;
+ALTER TABLE jdd DROP COLUMN export_file_id;
+ALTER TABLE jdd RENAME COLUMN dsr_updated_at TO data_updated_at;
+
+COMMENT ON COLUMN raw_data.jdd.id IS 'Technical id of the jdd';
+COMMENT ON COLUMN raw_data.jdd.status IS 'jdd status, can be ''empty'' (jdd created and active, no DSR nor DEE), ''active'' (at least the DSR or the DEE is created), ''deleted'' (deleted, but the row is kept)';
+COMMENT ON COLUMN raw_data.jdd.provider_id IS 'The data provider identifier (country code or organisation name)';
+COMMENT ON COLUMN raw_data.jdd.user_login IS 'The login of the user who created the jdd';
+COMMENT ON COLUMN raw_data.jdd.model_id IS 'Id of the data model in which the jdd is delivered';
+COMMENT ON COLUMN raw_data.jdd.created_at IS 'jdd creation timestamp (delivery timestamp is in submission._creationdt)';
+COMMENT ON COLUMN raw_data.jdd.data_updated_at IS 'DSR last edition timestamp';
+
---
+++ database/update/migrate_1.0.5_to_v2.0.0/update_db_v1_to_v2.php
@@ -0,0 +1,134 @@
+<?php
+$currentDir = dirname(__FILE__);
+require_once "$currentDir/../../../lib/share.php";
+
+// -----------------------------------------------------------------------------------------
+// Synopsis: migrate DB GINCO from sprint 30 to sprint 32
+// -----------------------------------------------------------------------------------------
+function usage($mess = NULL) {
+	echo "------------------------------------------------------------------------\n";
+	echo ("\nUpdate DB from sprint v1.0.5 (zend) to v2.0.0 (SF2)\n");
+	echo ("> php update_db_sprint.php -f <configFile> [{-D<propertiesName>=<Value>}]\n\n");
+	echo "o <configFile>: a java style properties file for the instance on which you work\n";
+	echo "o -D : inline options to complete or override the config file.\n";
+	echo "------------------------------------------------------------------------\n";
+	if (!is_null($mess)) {
+		echo ("$mess\n\n");
+		exit(1);
+	}
+	exit();
+}
+
+if (count($argv) == 1)
+	usage();
+$config = loadPropertiesFromArgs();
+
+//-- Sprint 25 - MInus migration of predefined requests
+//
+$sprintDir = realpath($currentDir . '/../sprint25');
+
+try {
+	// Zend 1 --> Symfony 2 migration
+	execCustSQLFile("$sprintDir/add_extension_pg_trgm.sql", $config);
+	execCustSQLFile("$currentDir/update_db_to_v4.sql", $config); // new one - without predefined requests migration
+	execCustSQLFile("$sprintDir/update_layers_and_layer_tree.sql", $config);
+
+	// Other
+	execCustSQLFile("$sprintDir/reorder_layer_tree.sql", $config);
+	execCustSQLFile("$sprintDir/add_integration_service_event_listener.sql", $config);
+
+} catch (Exception $e) {
+	echo "exception: " . $e->getMessage() . "\n";
+	exit(1);
+}
+
+//-- Sprint 26
+//
+// C'est bon rien à faire
+
+//-- Sprint 27
+//
+//// C'est bon rien à faire
+
+//-- Sprint 28 / v1.0.1
+//
+$sprintDir = realpath($currentDir . '/../sprint28');
+
+try {
+	execCustSQLFile("$sprintDir/update_users_table.sql", $config);
+} catch (Exception $e) {
+	echo "exception: " . $e->getMessage() . "\n";
+	exit(1);
+}
+
+
+//-- Sprint 29 / v1.0.2
+//
+$sprintDir = realpath($currentDir . '/../sprint29');
+
+try {
+	execCustSQLFile("$sprintDir/ref_deefloutagevalue.sql", $config);
+	execCustSQLFile("$sprintDir/update_bbox_params.sql", $config);
+	execCustSQLFile("$sprintDir/change_time_case.sql", $config);
+} catch (Exception $e) {
+	echo "exception: " . $e->getMessage() . "\n";
+	exit(1);
+}
+
+
+//-- Sprint 30 / v1.0.3
+//
+$sprintDir = realpath($currentDir . '/../sprint30');
+
+try {
+	execCustSQLFile("$sprintDir/add_event_listener_jdd_service.sql", $config);
+	execCustSQLFile("$sprintDir/add_event_listener_generate_reports_service.sql", $config);
+	execCustSQLFile("$sprintDir/update_bbox.sql", $config);
+} catch (Exception $e) {
+	echo "exception: " . $e->getMessage() . "\n";
+	exit(1);
+}
+
+
+//-- Migration of jdd and dee tables
+//
+try {
+	$sprintDir = realpath($currentDir . '/../sprint30');
+	execCustSQLFile("$sprintDir/add_messages_table.sql", $config); // DROP jobs and CREATE messages
+	execCustSQLFile("$currentDir/migrate_jdds_and_dee.sql", $config); // Migrate structure and datas
+} catch (Exception $e) {
+	echo "exception: " . $e->getMessage() . "\n";
+	exit(1);
+}
+
+
+//-- Migration of predefined requests with datas
+//
+try {
+	execCustSQLFile("$currentDir/migrate_jdds_and_dee.sql", $config); // Migrate structure and datas
+} catch (Exception $e) {
+	echo "exception: " . $e->getMessage() . "\n";
+	exit(1);
+}
+
+//-- Version update
+//
+try {
+	execCustSQLFile("$currentDir/version.sql", $config); // Migrate structure and datas
+} catch (Exception $e) {
+	echo "exception: " . $e->getMessage() . "\n";
+	exit(1);
+}
+
+$CLIParams = implode(' ', array_slice($argv, 1));
+
+/* patch user raw_data here */
+//  system("php $sprintDir/script1.php $CLIParams", $returnCode1);
+// system("php $sprintDir/script2.php $CLIParams", $returnCode2);
+/*
+if ($returnCode1 != 0 || $returnCode2 != 0) {
+	echo "$sprintDir/update_db_sprint.php\n";
+	echo "exception: " . $e->getMessage() . "\n";
+	exit(1);
+}
+*/
