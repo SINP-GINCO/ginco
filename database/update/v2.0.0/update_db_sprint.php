5f08d6c3c2513c2724805b04ebbd70aabb0444e0
scandel <severine.candelier@ign.fr>
2017-07-27 15:24:40 +0200

    Create v2.0.0

--- database/update/v2.0.0/update_db_sprint.php
+++ database/update/v2.0.0/update_db_sprint.php
@@ -3,11 +3,11 @@ $sprintDir = dirname(__FILE__);
 require_once "$sprintDir/../../../lib/share.php";
 
 // -----------------------------------------------------------------------------------------
-// Synopsis: migrate DB GINCO from sprint 30 to sprint 32
+// Synopsis: migrate DB GINCO from sprint 30 to v2.0.0 (Nothing to do because it's the same)
 // -----------------------------------------------------------------------------------------
 function usage($mess = NULL) {
 	echo "------------------------------------------------------------------------\n";
-	echo ("\nUpdate DB from sprint 30 to 32\n");
+	echo ("\nUpdate DB from sprint 30 to v2.0.0\n");
 	echo ("> php update_db_sprint.php -f <configFile> [{-D<propertiesName>=<Value>}]\n\n");
 	echo "o <configFile>: a java style properties file for the instance on which you work\n";
 	echo "o -D : inline options to complete or override the config file.\n";
