8c555b529ee5d826ab4321114a18276351d7d44c
amouget <anna.mouget@ign.fr>
2017-08-22 14:52:24 +0200

    #1125: Update bac with admin express 2017

--- database/init/3-Update_mapping_schema_tpl.sql
+++ database/init/3-Update_mapping_schema_tpl.sql
@@ -190,6 +190,6 @@ DELETE FROM bac_commune;
 DELETE FROM bac_departement;
 DELETE FROM bac_maille;
 -- Populate the vizualisation bacs with the referentiels tables
-INSERT INTO bac_commune SELECT r.insee_com, St_Transform(r.geom, 3857) FROM referentiels.geofla_commune AS r;
-INSERT INTO bac_departement SELECT r.code_dept, St_Transform(r.geom, 3857) FROM referentiels.geofla_departement AS r;
+INSERT INTO bac_commune SELECT r.insee_com, St_Transform(r.geom, 3857) FROM referentiels.commune_carto_2017 AS r;
+INSERT INTO bac_departement SELECT r.code_dept, St_Transform(r.geom, 3857) FROM referentiels.departement_carto_2017 AS r;
 INSERT INTO bac_maille SELECT r.code_10km, St_Transform(r.geom, 3857) FROM referentiels.codemaillevalue AS r;
