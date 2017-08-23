ed1aa2815891922251968e1a3d83a5f3daa65fee
amouget <anna.mouget@ign.fr>
2017-08-23 13:12:43 +0200

    #1125: Update commune codes in observation_commune table

--- database/update/v2.0.001/replace_geofla_commune_by_admin_express-cog_2017.sql
+++ database/update/v2.0.001/replace_geofla_commune_by_admin_express-cog_2017.sql
@@ -1,8 +1,9 @@
+-- 1) First delete constraint
 ALTER TABLE bac_commune DROP CONSTRAINT FK_bac_commune_geofla_commune;
 
 DROP TABLE referentiels.geofla_commune;
 
--- Update bac_commune
+-- 2) Update commune
 DELETE FROM bac_commune;
 -- Populate the vizualisation bacs with the referentiels tables
 ALTER TABLE bac_commune ALTER COLUMN geom SET DATA TYPE geometry;
@@ -12,11 +13,7 @@ INSERT INTO bac_commune SELECT r.insee_com, St_Transform(r.geom, 3857) FROM refe
 --ALTER TABLE bac_commune ALTER COLUMN geom 
 --    SET DATA TYPE geometry(MultiPolygon,3857) USING ST_Multi(geom);
 
-ALTER TABLE bac_commune ADD CONSTRAINT FK_bac_commune_commune_carto_2017
-FOREIGN KEY (id_commune) REFERENCES referentiels.commune_carto_2017 (insee_com)
-ON DELETE RESTRICT ON UPDATE CASCADE DEFERRABLE INITIALLY DEFERRED;
-
-
+-- Update dynamodes
 UPDATE metadata.dynamode SET sql='SELECT insee_com as code, insee_com as label, ''''::text as definition, ''''::text as position FROM referentiels.commune_carto_2017 ORDER BY insee_com' WHERE unit='CodeCommuneCalculeValue';
 UPDATE metadata.dynamode SET sql='SELECT insee_com as code, insee_com as label, ''''::text as definition, ''''::text as position FROM referentiels.commune_carto_2017 ORDER BY insee_com' WHERE unit='CodeCommuneValue';
 UPDATE metadata.dynamode SET sql='SELECT nom_com as code, nom_com || '' ('' || insee_com || '')'' as label, ''''::text as definition, ''''::text as position FROM referentiels.commune_carto_2017 ORDER BY nom_com' WHERE unit='NomCommuneCalculeValue';
@@ -24,3 +21,14 @@ UPDATE metadata.dynamode SET sql='SELECT nom_com as code, nom_com || '' ('' || i
 UPDATE metadata_work.dynamode SET sql='SELECT insee_com as code, insee_com as label, ''''::text as definition, ''''::text as position FROM referentiels.commune_carto_2017 ORDER BY insee_com' WHERE unit='CodeCommuneCalculeValue';
 UPDATE metadata_work.dynamode SET sql='SELECT insee_com as code, insee_com as label, ''''::text as definition, ''''::text as position FROM referentiels.commune_carto_2017 ORDER BY insee_com' WHERE unit='CodeCommuneValue';
 UPDATE metadata_work.dynamode SET sql='SELECT nom_com as code, nom_com || '' ('' || insee_com || '')'' as label, ''''::text as definition, ''''::text as position FROM referentiels.commune_carto_2017 ORDER BY nom_com' WHERE unit='NomCommuneCalculeValue';
+
+-- 3) Recreate constraints
+ALTER TABLE bac_commune ADD CONSTRAINT FK_bac_commune_commune_carto_2017
+FOREIGN KEY (id_commune) REFERENCES referentiels.commune_carto_2017 (insee_com)
+ON DELETE RESTRICT ON UPDATE CASCADE DEFERRABLE INITIALLY DEFERRED;
+
+--Add a forgotten foreign key 
+ALTER TABLE observation_commune ADD CONSTRAINT fk_observation_departement_id_departement FOREIGN KEY (id_departement)
+ REFERENCES mapping.bac_departement (id_departement) MATCH SIMPLE
+ ON UPDATE CASCADE ON DELETE RESTRICT DEFERRABLE INITIALLY DEFERRED;
+
--- database/update/v2.0.001/replace_geofla_region_by_admin_express-cog_2017.sql
+++ database/update/v2.0.001/replace_geofla_region_by_admin_express-cog_2017.sql
@@ -1,8 +1,7 @@
-ALTER TABLE bac_region DROP CONSTRAINT FK_bac_region_geofla_region,
-ADD CONSTRAINT FK_bac_region_region_carto_2017
-FOREIGN KEY (id_region) REFERENCES referentiels.region_carto_2017 (code_reg)
-ON DELETE RESTRICT ON UPDATE CASCADE DEFERRABLE INITIALLY DEFERRED;
+-- 1) First delete constraint
+ALTER TABLE bac_region DROP CONSTRAINT FK_bac_region_geofla_region;
 
+-- 2) Update departement
 DROP TABLE referentiels.geofla_region;
 
 -- Update bac_region
@@ -18,3 +17,8 @@ INSERT INTO bac_region SELECT r.code_reg, St_Transform(r.geom, 3857) FROM refere
 UPDATE metadata.dynamode SET sql='SELECT code_reg as code, nom_reg || '' ('' || code_reg || '')'' as label, ''''::text as definition, ''''::text as position  FROM referentiels.region_carto_2017 ORDER BY code_reg' WHERE unit='region';
 
 UPDATE metadata_work.dynamode SET sql='SELECT code_reg as code, nom_reg || '' ('' || code_reg || '')'' as label, ''''::text as definition, ''''::text as position  FROM referentiels.region_carto_2017 ORDER BY code_reg' WHERE unit='region';
+
+-- 3) Recreate constraint
+ALTER TABLE bac_region ADD CONSTRAINT FK_bac_region_region_carto_2017
+FOREIGN KEY (id_region) REFERENCES referentiels.region_carto_2017 (code_reg)
+ON DELETE RESTRICT ON UPDATE CASCADE DEFERRABLE INITIALLY DEFERRED;
