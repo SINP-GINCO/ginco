8c741555d34d33a59e001c2fae336d03aabccc52
Gautam <gautam.pastakia@ign.fr>
2017-09-01 15:21:53 +0200

    #1256: Fix update script

--- database/update/v2.0.002/update_role_visiteur_to_grand_public.sql
+++ database/update/v2.0.002/update_role_visiteur_to_grand_public.sql
@@ -4,5 +4,4 @@ UPDATE role_to_user SET role_code = 'grand_public' WHERE user_login = 'visiteur'
 
 UPDATE role_to_schema SET role_code = 'grand_public' WHERE role_code = 'visiteur';
 
-INSERT INTO permission_per_role(role_code, permission_code) VALUES ('grand_public', 'DATA_QUERY');
-INSERT INTO permission_per_role(role_code, permission_code) VALUES ('grand_public', 'DATA_QUERY_OTHER_PROVIDER');
+UPDATE permission_per_role SET role_code = 'grand_public' WHERE role_code = 'visiteur';
