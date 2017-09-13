eb392664b5ed2c4dcb9f8f5d1dfd3264adf33371
amouget <anna.mouget@ign.fr>
2017-09-13 15:10:38 +0200

    #1127: Test database dump restore system

---
+++ database/update/v2.0.002/script_with_problem_to_test_restore.php
@@ -0,0 +1,20 @@
+<?php
+$sprintDir = dirname(__FILE__);
+require_once "$sprintDir/../../../lib/share.php";
+
+try {
+	
+	$config = loadPropertiesFromArgs();
+	$conn_string = "host={$config['db.host']} port={$config['db.port']} user={$config['db.adminuser']} password={$config['db.adminuser.pw']} dbname={$config['db.name']}";
+	$dbconn = pg_connect($conn_string) or die('Connection failed');
+	
+	$sqlCodeWithError = "SELECT erreur FROM erreur;";
+	
+	pg_query($sqlCodeWithError);
+	
+	pg_close($dbconn);
+} catch (Exception $e) {
+	echo "$sprintDir/update_db_sprint.php\n";
+	echo "exception: " . $e->getMessage() . "\n";
+	exit(1);
+}
