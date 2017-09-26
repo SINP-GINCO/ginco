35bc98999d234cced3f2779d21c90f5059129ea1
Gautam <gautam.pastakia@ign.fr>
2017-09-26 14:16:23 +0200

    #972: Format code

--- website/server/src/Ign/Bundle/GincoBundle/Controller/IntegrationController.php
+++ website/server/src/Ign/Bundle/GincoBundle/Controller/IntegrationController.php
@@ -29,20 +29,20 @@ class IntegrationController extends BaseController {
 	 * @Route("/create-data-submission", name="integration_creation")
 	 */
 	public function createDataSubmissionAction(Request $request) {
-		
+
 		// Get the referer url, and put it in session to redirect to it at the end of the process
 		$refererUrl = $request->headers->get('referer');
 		$redirectUrl = ($refererUrl) ? $refererUrl : $this->generateUrl('integration_home');
 		$session = $request->getSession();
 		if (!$session->has('redirectToUrl'))
 			$session->set('redirectToUrl', $redirectUrl);
-		
+
 		$em = $this->get('doctrine.orm.entity_manager');
-		
+
 		// Find jddid if given in GET parameters
 		$jddId = intval($request->query->get('jddid', 0));
 		$jdd = $em->getRepository('OGAMBundle:RawData\Jdd')->findOneById($jddId);
-		
+
 		// If the model of the jdd has no published datasets, add a flash error message
 		// And disable the whole form
 		$formDisabled = false;
@@ -59,13 +59,13 @@ class IntegrationController extends BaseController {
 				->getProvider(),
 			'disabled' => $formDisabled
 		));
-		
+
 		$form->handleRequest($request);
-		
+
 		if ($form->isSubmitted() && $form->isValid()) {
 			// Add user relationship
 			$submission->setUser($this->getUser());
-			
+
 			// Add jdd relationship
 			// And update jdd "dataUpdatedAt"
 			if ($form->has('jddid')) {
@@ -75,19 +75,19 @@ class IntegrationController extends BaseController {
 				$jdd->setDataUpdatedAt(new \DateTime());
 				$em->merge($jdd);
 			}
-			
+
 			// writes the submission to the database
 			// merge because cascade persist is not set in the entity
 			// and get the merged object to access auto-generated id
 			$attachedSubmission = $em->merge($submission);
 			$em->flush();
-			
+
 			// Redirects to page 2 of the form: upload data
 			return $this->redirect($this->generateUrl('integration_upload_data', array(
 				'id' => $attachedSubmission->getId()
 			)));
 		}
-		
+
 		return $this->render('IgnGincoBundle:Integration:show_create_data_submission.html.twig', array(
 			'form' => $form->createView()
 		));
@@ -103,16 +103,16 @@ class IntegrationController extends BaseController {
 	 */
 	public function validateDataAction(Request $request) {
 		$this->getLogger()->debug('validateDataAction');
-		
+
 		// Get the submission Id
 		$submissionId = $request->get("submissionId");
-		
+
 		// Send the validation request to the integration server
 		try {
 			$this->get('ogam.integration_service')->validateDataSubmission($submissionId);
 		} catch (\Exception $e) {
 			$this->getLogger()->error('Error during upload: ' . $e);
-			
+
 			return $this->render('OGAMBundle:Integration:data_error.html.twig', array(
 				'error' => $this->get('translator')
 					->trans("An unexpected error occurred.")
@@ -122,10 +122,10 @@ class IntegrationController extends BaseController {
 		$submissionRepo = $this->getDoctrine()->getRepository('Ign\Bundle\OGAMBundle\Entity\RawData\Submission', 'raw_data');
 		$submission = $submissionRepo->find($submissionId);
 		$jddMetadataId = $submission->getJdd()->getField('metadataId');
-		
+
 		// -- Send the email
 		$siteName = $this->get('ogam.configuration_manager')->getConfig('site_name');
-		
+
 		// Files of the submission
 		$submissionFiles = $submission->getFiles();
 		$fileNames = array();
@@ -133,21 +133,21 @@ class IntegrationController extends BaseController {
 			$fileName = basename($submissionFile->getFileName());
 			$fileNames[] = $fileName;
 		}
-		
+
 		// Get recipient, the connected user.
 		$user = $this->getUser();
-		
+
 		// Title and body:
 		$title = (count($fileNames) > 1) ? "Intégration des fichiers " : "Intégration du fichier ";
 		$title .= implode($fileNames, ", ");
-		
+
 		// -- Attachments
 		$reports = $this->get('ginco.submission_service')->getReportsFilenames($submissionId);
 		$attachements = array();
-		
+
 		// Regenerate sensibility report each time (see #815)
 		$this->get('ginco.submission_service')->generateReport($submissionId, "sensibilityReport");
-		
+
 		foreach ($reports as $report => $reportPath) {
 			// Regenerate report if does not exist
 			if (!is_file($reportPath)) {
@@ -159,7 +159,7 @@ class IntegrationController extends BaseController {
 			}
 			$attachements[] = $reportPath;
 		}
-		
+
 		$this->get('app.mail_manager')->sendEmail('IgnGincoBundle:Emails:publication-notification-to-user.html.twig', array(
 			'metadata_uuid' => $jddMetadataId,
 			'user' => $user,
