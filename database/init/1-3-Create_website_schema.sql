67adf41be89b14499375cad5096f256df0f6f892
scandel <severine.candelier@ign.fr>
2017-10-03 11:23:08 +0200

    #1220: Create Content Entity and table

--- database/init/1-3-Create_website_schema.sql
+++ database/init/1-3-Create_website_schema.sql
@@ -301,6 +301,24 @@ GRANT ALL ON TABLE website.messages TO admin;
 GRANT ALL ON TABLE website.messages TO ogam;
 
 
+-- Table: website.content
+
+-- DROP TABLE website.content;
+
+CREATE TABLE website.content (
+  name VARCHAR(50) NOT NULL,
+  value TEXT DEFAULT NULL,
+  description VARCHAR(500) DEFAULT NULL,
+  PRIMARY KEY(name)
+);
+CREATE UNIQUE INDEX UNIQ_3211DD035E237E06 ON website.content (name);
+
+ALTER TABLE website.content
+  OWNER TO admin;
+GRANT ALL ON TABLE website.content TO admin;
+GRANT ALL ON TABLE website.content TO ogam;
+
+
 GRANT ALL ON SCHEMA website TO ogam WITH GRANT OPTION;
 GRANT ALL ON ALL SEQUENCES IN SCHEMA website TO ogam;
 GRANT ALL ON ALL TABLES IN SCHEMA website TO ogam;
