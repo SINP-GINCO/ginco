9c02976435c3559ecd00eb02b427f400373e00fb
scandel <severine.candelier@ign.fr>
2017-10-18 17:40:32 +0200

    #1180:  Clean CASAuthenticator code

---
+++ website/server/src/Ign/Bundle/GincoBundle/Exception/CASException.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Ign\Bundle\GincoBundle\Exception;
+
+/**
+ * Class CASException
+ * Exception occuring when the application can't access the INPN CAS
+ *
+ * @package Ign\Bundle\GincoBundle\Exception
+ */
+class CASException extends \Exception implements IgnGincoBundleExceptionInterface
+{
+}
