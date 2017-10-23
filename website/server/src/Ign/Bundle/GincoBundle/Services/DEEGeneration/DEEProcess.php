3efe1d7516282804eb3ff4b2e830cdad6a13d3a7
amouget <anna.mouget@ign.fr>
2017-10-23 17:40:38 +0200

    #1283: Send mails at the end of dlb generation process

--- website/server/src/Ign/Bundle/GincoBundle/Services/DEEGeneration/DEEProcess.php
+++ website/server/src/Ign/Bundle/GincoBundle/Services/DEEGeneration/DEEProcess.php
@@ -27,36 +27,46 @@ use Symfony\Bridge\Monolog\Logger;
 class DEEProcess {
 
 	/**
+	 *
 	 * @var EntityManager
 	 */
 	protected $em;
-	
+
 	/**
+	 *
 	 * @var ConfigurationManager
 	 */
 	protected $configuration;
 
 	/**
+	 *
 	 * @var DEEGenerator
 	 */
 	protected $DEEGenerator;
 
 	/**
+	 *
 	 * @var MailManager
 	 */
 	protected $mailManager;
 
 	/**
+	 *
 	 * @var Logger
 	 */
 	protected $logger;
 
 	/**
 	 * DEEProcess constructor.
-	 * @param $em
-	 * @param $configuration
-	 * @param $mailManager
-	 * @param $logger
+	 *
+	 * @param
+	 *        	$em
+	 * @param
+	 *        	$configuration
+	 * @param
+	 *        	$mailManager
+	 * @param
+	 *        	$logger
 	 */
 	public function __construct($em, $configuration, $DEEGenerator, $mailManager, $logger) {
 		$this->em = $em;
@@ -70,36 +80,37 @@ class DEEProcess {
 	 * Create an entry in the DEE table
 	 * The new DEE still needs to be generated in a file
 	 *
-	 * @param Jdd $jdd
-	 * @param User $user
-	 * @param $comment
+	 * @param Jdd $jdd        	
+	 * @param User $user        	
+	 * @param
+	 *        	$comment
 	 * @return DEE
 	 */
 	public function createDEELine(Jdd $jdd, User $user, $comment) {
-
+		
 		// Get last version of DEE attached to the jdd
 		$deeRepo = $this->em->getRepository('IgnGincoBundle:RawData\DEE');
 		$lastDEE = $deeRepo->findLastVersionByJdd($jdd);
 		$lastVersion = ($lastDEE) ? $lastDEE->getVersion() : 0;
-
+		
 		// Create a new DEE version and attach it to the jdd
 		$newDEE = new DEE();
 		$newDEE->setJdd($jdd)
 			->setVersion($lastVersion + 1)
 			->setComment($comment)
 			->setUser($user);
-
+		
 		// Set submissions ids (validated submissions)
 		$submissionIds = array();
 		foreach ($jdd->getValidatedSubmissions() as $submission) {
 			$submissionIds[] = $submission->getId();
 		}
 		$newDEE->setSubmissions($submissionIds);
-
+		
 		// Persist in db and return the DEE object
 		$this->em->persist($newDEE);
 		$this->em->flush();
-
+		
 		return $newDEE;
 	}
 
@@ -107,19 +118,18 @@ class DEEProcess {
 	 * Remove a DEE line from db and generated files if needed
 	 * (case where DEE generation is cancelled)
 	 *
-	 * @param DEE $DEE
+	 * @param DEE $DEE        	
 	 */
 	public function deleteDEELineAndFiles($DEEId, $fileName = null) {
-
 		$this->logger->info("deleteDEELineAndFiles: " . implode(',', func_get_args()));
 		$DEE = $this->em->getRepository('IgnGincoBundle:RawData\DEE')->findOneById($DEEId);
-
+		
 		// Delete GML archive
 		if ($DEE->getFilepath()) {
 			$archive = $this->configuration->getConfig('deePublicDirectory') . '/' . pathinfo($DEE->getFilepath(), PATHINFO_BASENAME);
 			unlink($archive);
 		}
-
+		
 		// Delete intermediate files
 		if ($fileName) {
 			$dir = dirname($fileName);
@@ -128,7 +138,7 @@ class DEEProcess {
 				rmdir($dir);
 			}
 		}
-
+		
 		if ($DEE) {
 			$this->em->remove($DEE);
 			$this->em->flush();
@@ -138,7 +148,8 @@ class DEEProcess {
 	/**
 	 * Refresh message from db and test if status is TO_CANCEL
 	 *
-	 * @param $message
+	 * @param
+	 *        	$message
 	 * @return bool
 	 */
 	protected function messageToCancel($message) {
@@ -154,84 +165,84 @@ class DEEProcess {
 	/**
 	 * Whole process for generating the DEE and sending notifications to MNHN and INPN
 	 *
-	 * @param $DEEId
-	 * @param null $messageId
+	 * @param
+	 *        	$DEEId
+	 * @param null $messageId        	
 	 */
 	public function generateAndSendDEE($DEEId, $messageId = null, $notifyUser = true) {
 		$this->logger->info("GenerateAndSendDEE: " . implode(',', func_get_args()));
-
+		
 		// Get all objects and variables
 		$DEE = $this->em->getRepository('IgnGincoBundle:RawData\DEE')->findOneById($DEEId);
-
+		
 		if ($DEE) {
 			// RabbitMQ Message if given
 			$message = ($messageId) ? $this->em->getRepository('IgnGincoBundle:Website\Message')->findOneById($messageId) : null;
-
+			
 			// Filepath of the DEE gml file
 			$DEEfilePath = $this->generateFilePath($DEE);
-
+			
 			if (!$this->messageToCancel($message)) {
 				// Generate the DEE gml file
 				$this->DEEGenerator->generateDeeGml($DEE, $DEEfilePath, $message);
 			}
-
+			
 			if (!$this->messageToCancel($message)) {
 				// Generate the DEE archive and set the download path of the DEE
 				$archiveFileSystemPath = $this->createDEEArchive($DEE, $DEEfilePath);
 				$archiveDownloadPath = '/dee/' . pathinfo($archiveFileSystemPath, PATHINFO_BASENAME);
 				$DEE->setFilePath($archiveDownloadPath);
 			}
-
+			
 			if (!$this->messageToCancel($message)) {
 				// Report message status if not null
 				if ($message) {
 					$message->setStatus(Message::STATUS_COMPLETED);
 				}
-
+				
 				// Set final statuses on DEE
 				$DEE->setStatus(DEE::STATUS_OK);
 				$this->em->flush();
 			}
-
-			if (!$this->messageToCancel($message)) {
-				// Send mail notifications to MNHN and user
-				$this->sendDEENotificationMail($DEE, $notifyUser);
+			
+			if ($notifyUser) {
+				if (!$this->messageToCancel($message)) {
+					// Send mail notifications to MNHN and user
+					$this->sendDEENotificationMail($DEE, $notifyUser);
+				}
 			}
-
+			
 			// Cancel message and delete DEE if needed
 			if ($this->messageToCancel($message)) {
 				$this->deleteDEELineAndFiles($DEE->getId(), $DEEfilePath);
-
+				
 				$message->setStatus(Message::STATUS_CANCELLED);
 				$this->em->flush();
 				echo "Message cancelled... DEE deleted.\n";
 			}
-
 		}
 	}
-
 
 	/**
 	 * Create the filepath of the DEE gml file
 	 *
-	 * @param Jdd $jdd
+	 * @param Jdd $jdd        	
 	 * @return string
 	 */
-    public function generateFilePath(DEE $DEE) {
-        $regionCode = $this->configuration->getConfig('regionCode','REGION');
-        $date = $DEE->getCreatedAt()->format('Y-m-d_H-i-s');
+	public function generateFilePath(DEE $DEE) {
+		$regionCode = $this->configuration->getConfig('regionCode', 'REGION');
+		$date = $DEE->getCreatedAt()->format('Y-m-d_H-i-s');
 		$jdd = $DEE->getJdd();
-
+		
 		$uuid = $jdd->getField('metadataId', $jdd->getId());
-
-        $fileNameWithoutExtension = $regionCode . '_' . $date . '_' . $uuid ;
-
-        $filePath = $this->configuration->getConfig('deePrivateDirectory') . '/' . $fileNameWithoutExtension . '/';
-        $filename = $fileNameWithoutExtension . '.xml';
-
-        return $filePath . $filename ;
-    }
-
+		
+		$fileNameWithoutExtension = $regionCode . '_' . $date . '_' . $uuid;
+		
+		$filePath = $this->configuration->getConfig('deePrivateDirectory') . '/' . $fileNameWithoutExtension . '/';
+		$filename = $fileNameWithoutExtension . '.xml';
+		
+		return $filePath . $filename;
+	}
 
 	/**
 	 * Create a gzipped archive containing:
@@ -239,24 +250,26 @@ class DEEProcess {
 	 * - informations on the jdd and the dee generation
 	 * And put the archive in the "deePublicDirectory" from conf (in the public directory).
 	 *
-	 * @param $jddId
-	 * @param $fileName string file of the DEE
-	 * @param $comment
+	 * @param
+	 *        	$jddId
+	 * @param $fileName string
+	 *        	file of the DEE
+	 * @param
+	 *        	$comment
 	 * @return string
 	 */
-	public function createDEEArchive(DEE $DEE, $fileName)
-	{
+	public function createDEEArchive(DEE $DEE, $fileName) {
 		// Get fileName without extension
 		$filePath = pathinfo($fileName, PATHINFO_DIRNAME);
 		$fileNameWithoutExtension = pathinfo($fileName, PATHINFO_FILENAME);
-
+		
 		// Public directory to store the archive
 		$deePublicDir = $this->configuration->getConfig('deePublicDirectory');
-
+		
 		// JDD metadata id
 		$jdd = $DEE->getJdd();
 		$uuid = $jdd->getField('metadataId', '');
-
+		
 		$descriptionFile = $filePath . '/descriptif.txt';
 		try {
 			$out = fopen($descriptionFile, 'w');
@@ -264,44 +277,41 @@ class DEEProcess {
 			// Todo: rajouter les dates, la version, et l'action (creation, update, delete)
 			fwrite($out, "Raisons amenant à une génération ou regénération de la DEE :\n " . stripslashes($DEE->getComment()));
 			fclose($out);
-		}
-		catch (\Exception $e) {
+		} catch (\Exception $e) {
 			throw new DEEException("Could not wite to $descriptionFile: " . $e->getMessage());
 		}
-
+		
 		// Create an archive of the whole directory
 		$parentDir = dirname($filePath); // deePrivateDirectory
 		$archiveName = $deePublicDir . '/' . $fileNameWithoutExtension . '.zip';
 		try {
 			chdir($parentDir);
 			system("zip -r $archiveName $fileNameWithoutExtension");
-		}
-		catch (\Exception $e) {
+		} catch (\Exception $e) {
 			throw new DEEException("Could not create archive $archiveName:" . $e->getMessage());
 		}
 		// Delete the other files
 		unlink($descriptionFile);
-
+		
 		// Delete GML file
 		unlink($fileName);
 		rmdir($filePath);
-
+		
 		return $archiveName;
 	}
 
-
 	/**
 	 * Send two notification emails after creation of the DEE archive:
-	 *  - one to the MNHN
-	 *  - one to the user who created the DEE (except if the DEE is being deleted)
+	 * - one to the MNHN
+	 * - one to the user who created the DEE (except if the DEE is being deleted)
 	 *
-	 * @param DEE $DEE the DEE object
+	 * @param DEE $DEE
+	 *        	the DEE object
 	 */
 	public function sendDEENotificationMail(DEE $DEE, $notifyUser) {
-
 		$jdd = $DEE->getJdd();
 		$user = $DEE->getUser();
-
+		
 		// DEE action : create or update
 		if ($DEE->getStatus() == DEE::STATUS_DELETED) {
 			$action = 'DEE.suppression';
@@ -309,69 +319,58 @@ class DEEProcess {
 			$action = 'DEE.creation';
 		} else {
 			$previousDEE = $this->em->getRepository('IgnGincoBundle:RawData\DEE')->findOneBy(array(
-					'jdd' => $jdd,
-					'version' => $DEE->getVersion() - 1
-				)
-			);
+				'jdd' => $jdd,
+				'version' => $DEE->getVersion() - 1
+			));
 			$action = ($previousDEE->getStatus() == DEE::STATUS_DELETED) ? 'DEE.creation' : 'DEE.update';
 		}
-
-
+		
 		// Parameters for email notifications
 		$parameters = array(
 			'action' => $action,
-			'region' => $this->configuration->getConfig('regionCode','REGION'),
+			'region' => $this->configuration->getConfig('regionCode', 'REGION'),
 			'metadata_uuid' => $jdd->getField('metadataId'),
 			'jdd_title' => $jdd->getField('title'),
 			'user' => $user,
 			'provider' => $jdd->getProvider(),
 			'message' => $DEE->getComment(),
-			'created' => $DEE->getCreatedAt(),
+			'created' => $DEE->getCreatedAt()
 		);
-
+		
 		if ($action != 'DEE.suppression') {
-
+			
 			// Submission and submission files in the jdd
 			$submissionFilesNames = array();
 			$submissionRepo = $this->em->getRepository('OGAMBundle:RawData\Submission');
-
+			
 			$submissionIds = $DEE->getSubmissions();
-
+			
 			foreach ($submissionIds as $submissionId) {
 				$submission = $submissionRepo->findOneById($submissionId);
-				foreach	( $submission->getFiles() as $file ) {
+				foreach ($submission->getFiles() as $file) {
 					$submissionFilesNames[] = $file->getFileName();
 				}
 			}
-			$fileNames = array_map("basename",$submissionFilesNames);
-
+			$fileNames = array_map("basename", $submissionFilesNames);
+			
 			// Checksum of the DEE archive
 			$deePublicDir = $this->configuration->getConfig('deePublicDirectory');
 			$fileName = pathinfo($DEE->getFilePath(), PATHINFO_BASENAME);
 			$checksum = md5_file($deePublicDir . '/' . $fileName);
-
+			
 			$parameters += array(
-				'filename' => implode(', ',$fileNames),
+				'filename' => implode(', ', $fileNames),
 				'download_url' => $this->configuration->getConfig('site_url') . $DEE->getFilePath(),
-				'checksum' => $checksum,
+				'checksum' => $checksum
 			);
 		}
-
+		
 		// Send mail notification to MNHN
-		$this->mailManager->sendEmail(
-			'IgnGincoBundle:Emails:DEE-notification-to-MNHN.html.twig',
-			$parameters,
-			$this->configuration->getConfig('deeNotificationMail','sinp-dev@ign.fr')
-		);
-
+		$this->mailManager->sendEmail('IgnGincoBundle:Emails:DEE-notification-to-MNHN.html.twig', $parameters, $this->configuration->getConfig('deeNotificationMail', 'sinp-dev@ign.fr'));
+		
 		// Send mail notification to user
 		if ($action != 'DEE.suppression' && $notifyUser) {
-			$this->mailManager->sendEmail(
-				'IgnGincoBundle:Emails:DEE-notification-to-user.html.twig',
-				$parameters,
-				$user->getEmail()
-			);
+			$this->mailManager->sendEmail('IgnGincoBundle:Emails:DEE-notification-to-user.html.twig', $parameters, $user->getEmail());
 		}
 	}
-
 }
