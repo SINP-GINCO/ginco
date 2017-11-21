edec9da021990a46726eeac4951592f34bfc9ca2
scandel <severine.candelier@ign.fr>
2017-11-21 16:25:22 +0100

    #1180:  Correct  fix for reverse proxy

--- website/server/web/app.php
+++ website/server/web/app.php
@@ -9,20 +9,9 @@ include_once __DIR__.'/../app/bootstrap.php.cache';
 $kernel = new AppKernel('prod', false);
 $kernel->loadClassCache();
 //$kernel = new AppCache($kernel);
-
-// todo : as soon as the reverse proxy pb is fixed, remove this and replace
-// todo : with correct trusted_proxies configuration
-if (isset($_SERVER['HTTP_X_FORWARDED_PROTO'])) {
-	$_SERVER['REQUEST_SCHEME'] = $_SERVER['HTTP_X_FORWARDED_PROTO'];
-	if ($_SERVER['HTTP_X_FORWARDED_PROTO'] == 'https') {
-		$_SERVER['HTTPS'] = 'on';
-	};
-}
 // When using the HttpCache, you need to call the method in your front controller instead of relying on the configuration parameter
 //Request::enableHttpMethodParameterOverride();
 $request = Request::createFromGlobals();
-
-
 $response = $kernel->handle($request);
 $response->send();
 $kernel->terminate($request, $response);
