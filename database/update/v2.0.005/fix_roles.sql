51746a6783d038fe595d792f9f871b1147501c81
scandel <severine.candelier@ign.fr>
2017-12-11 13:31:04 +0100

    Another Fix update script

--- database/update/v2.0.005/fix_roles.sql
+++ database/update/v2.0.005/fix_roles.sql
@@ -1,1 +1,6 @@
+INSERT INTO website.users(user_login, provider_id, email) VALUES
+  ('hlambert',1,'helene.lambert@ign.fr');
+INSERT INTO website.role_to_user(user_login, role_code) VALUES
+  ('hlambert', 1);
+
 UPDATE website.role_to_user SET role_code=1 WHERE user_login IN ('jpanijel','nbotte');
