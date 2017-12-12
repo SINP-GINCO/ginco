23236c2e683405dfeead950a84d1095954c0e130
scandel <severine.candelier@ign.fr>
2017-12-12 09:41:14 +0100

    Fix update script

--- database/update/v2.0.005/fix_roles.sql
+++ database/update/v2.0.005/fix_roles.sql
@@ -1,6 +1,9 @@
-INSERT INTO website.users(user_login, provider_id, email) VALUES
-  ('hlambert',1,'helene.lambert@ign.fr');
-INSERT INTO website.role_to_user(user_login, role_code) VALUES
-  ('hlambert', 1);
+INSERT INTO website.users(user_login, provider_id, email)
+  SELECT 'hlambert',1,'helene.lambert@ign.fr'
+  WHERE NOT EXISTS (SELECT user_login FROM website.users WHERE user_login = 'hlambert');
+
+INSERT INTO website.role_to_user(user_login, role_code)
+    SELECT 'hlambert', 1
+      WHERE NOT EXISTS (SELECT user_login FROM website.role_to_user WHERE user_login = 'hlambert');
 
 UPDATE website.role_to_user SET role_code=1 WHERE user_login IN ('jpanijel','nbotte');
