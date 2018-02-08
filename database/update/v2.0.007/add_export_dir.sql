4a781e9fa59953c6253dc6cfa5877b47d6baf61a
amouget <anna.mouget@ign.fr>
2018-02-08 11:14:07 +0100

    #1065: Fix update database script

--- database/update/v2.0.007/add_export_dir.sql
+++ database/update/v2.0.007/add_export_dir.sql
@@ -1,3 +1,3 @@
 INSERT INTO website.application_parameters(
             name, value, description)
-    VALUES ('exportPublicDirectory', '/var/data/ginco/localhostV4/export', 'Directory where asynchronous exports are stored and can be downloaded');
+    VALUES ('exportPublicDirectory', '/var/data/ginco/@instance.name@/export', 'Directory where asynchronous exports are stored and can be downloaded');
