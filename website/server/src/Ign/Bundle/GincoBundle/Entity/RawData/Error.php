9e5e49f38fb083c5c4a8664ec75e2092c7d2e9d7
cgimazane <clement.gimazane@ign.fr>
2018-04-11 19:33:07 +0200

    #1409: creating html view of error report

--- website/server/src/Ign/Bundle/GincoBundle/Entity/RawData/Error.php
+++ website/server/src/Ign/Bundle/GincoBundle/Entity/RawData/Error.php
@@ -6,125 +6,238 @@ use Doctrine\ORM\Mapping as ORM;
 /**
  * SubmissionFile.
  *
- * @ORM\Table(name="raw_data.submission_file")
- * @ORM\Entity
+ * @ORM\Table(name="raw_data.check_error")
+ * @ORM\Entity(repositoryClass="Ign\Bundle\GincoBundle\Repository\RawData\ErrorRepository")
  */
-class SubmissionFile {
+class Error {
+    
+        /**
+         * @ORM\Column(name="check_error_id")
+         * @ORM\Id
+         * 
+         * @var integer 
+         */    
+        private $id;
 
 	/**
-	 * @ORM\ManyToOne(targetEntity="Submission", inversedBy="files")
+	 * @ORM\ManyToOne(targetEntity="Submission")
 	 * @ORM\JoinColumn(name="submission_id", referencedColumnName="submission_id")
 	 * @ORM\Id
 	 */
 	private $submission;
+        
+        /**
+         * @ORM\Column(name="line_number")
+         * 
+         * @var integer 
+         */
+        private $lineNumber;
+        
+        /**
+         * @ORM\Column(name="src_data")
+         * 
+         * @var string 
+         */        
+        private $field;
 
-	/**
-	 * The type of the file (reference a DATASET_FILES.FORMAT).
-	 * 
-	 * @var string @ORM\Column(name="file_type", type="string", length=36)
-	 *      @ORM\Id
+        /**
+         * @ORM\ManyToOne(targetEntity="\Ign\Bundle\GincoBundle\Entity\Metadata\Check")
+         * @ORM\JoinColumn(name="check_id", referencedColumnName="check_id")
+         * @ORM\Id
+         * 
+         * @var integer 
+         */
+        private $error;
+
+        /**
+         * @ORM\Column(name="found_value")
+         * 
+         * @var string 
+         */
+        private $foundValue;
+
+        /**
+         * @ORM\Column(name="expected_value")
+         * 
+         * @var string 
+         */
+        private $expectedValue;
+
+        /**
+         * @ORM\Column(name="error_message")
+         * 
+         * @var string 
+         */
+        private $comment;
+        
+        /**
+	 * @ORM\Column(name="_creationdt", type="date")
+         * 
+	 * @var \DateTime 
 	 */
-	private $fileType;
+	private $creationDate;
+        
+        /**
+         * 
+         * @return integer
+         */
+        function getId() {
+            return $this->id;
+        }
 
-	/**
-	 * The name of the file.
-	 * 
-	 * @var string @ORM\Column(name="file_name", type="string", length=4000)
-	 */
-	private $fileName;
-
-	/**
-	 * The number of lines in the file.
-	 * 
-	 * @var int @ORM\Column(name="nb_line", type="integer", nullable=true)
-	 */
-	private $nbLines;
-
-	/**
-	 * Set fileType
-	 *
-	 * @param string $fileType        	
-	 *
-	 * @return SubmissionFile
-	 */
-	public function setFileType($fileType) {
-		$this->fileType = $fileType;
-		
-		return $this;
-	}
-
-	/**
-	 * Get fileType
-	 *
-	 * @return string
-	 */
-	public function getFileType() {
-		return $this->fileType;
-	}
-
-	/**
-	 * Set fileName
-	 *
-	 * @param string $fileName        	
-	 *
-	 * @return SubmissionFile
-	 */
-	public function setFileName($fileName) {
-		$this->fileName = $fileName;
-		
-		return $this;
-	}
-
-	/**
-	 * Get fileName
-	 *
-	 * @return string
-	 */
-	public function getFileName() {
-		return $this->fileName;
-	}
-
-	/**
-	 * Set nbLines
-	 *
-	 * @param integer $nbLines        	
-	 *
-	 * @return SubmissionFile
-	 */
-	public function setNbLines($nbLines) {
-		$this->nbLines = $nbLines;
-		
-		return $this;
-	}
-
-	/**
-	 * Get nbLines
-	 *
-	 * @return int
-	 */
-	public function getNbLines() {
-		return $this->nbLines;
-	}
-
-	/**
-	 * Set submission
-	 *
-	 * @param Submission $submission
-	 *
-	 * @return SubmissionFile
-	 */
-	public function setSubmission(Submission $submission) {
-		$this->submission = $submission;
-		
-		return $this;
-	}
-
-	/**
-	 * Get submission
+        /**
+         * 
+         * @param integer $id
+         * @return Error
+         */
+        function setId($id) {
+            $this->id = $id;
+            return $this;
+        }
+       
+        /**
 	 *
 	 * @return Submission
 	 */
 	public function getSubmission() {
-		return $this->submission;
+            return $this->submission;
 	}
+        
+	/**
+	 *
+	 * @param Submission $submission
+	 * @return Error
+	 */
+	public function setSubmission(Submission $submission) {
+            $this->submission = $submission;		
+            return $this;
+	}
+
+        /**
+         * 
+         * @return integer
+         */
+        function getLineNumber() {
+            return $this->lineNumber;
+        }
+
+        /**
+         * 
+         * @param integer $lineNumber
+         * @return Error
+         */
+        function setLineNumber($lineNumber) {
+            $this->lineNumber = $lineNumber;
+            return $this;
+        }
+
+        /**
+         * 
+         * @return string
+         */
+        function getField() {
+            return $this->field;
+        }
+
+        /**
+         * 
+         * @param string $field
+         * @return Error
+         */
+        function setField($field) {
+            $this->field = $field;
+            return $this;
+        }
+
+                
+        /**
+         * 
+         * @return integer
+         */
+        function getError() {
+            return $this->error;
+        }
+
+        /**
+         * 
+         * @param integer $error
+         * @return Error
+         */
+        function setError($error) {
+            $this->error = $error;
+            return $this;
+        }
+
+        /**
+         * 
+         * @return \DateTime
+         */
+        function getCreationDate() {
+            return $this->creationDate;
+        }
+
+        /**
+         * 
+         * @param \DateTime $creationDate
+         * @return Error
+         */
+        function setCreationDate(\DateTime $creationDate) {
+            $this->creationDate = $creationDate;
+            return $this;
+        }
+
+        /**
+         * 
+         * @return string
+         */
+        function getFoundValue() {
+            return $this->foundValue;
+        }
+
+        /**
+         * 
+         * @param string $foundValue
+         * @return $this
+         */
+        function setFoundValue($foundValue) {
+            $this->foundValue = $foundValue;
+            return $this;
+        }
+
+        /**
+         * 
+         * @return string
+         */
+        function getExpectedValue() {
+            return $this->expectedValue;
+        }
+
+        /**
+         * 
+         * @param string $expectedValue
+         * @return Error
+         */
+        function setExpectedValue($expectedValue) {
+            $this->expectedValue = $expectedValue;
+            return $this;
+        }
+
+        /**
+         * 
+         * @return string
+         */
+        function getComment() {
+            return $this->comment;
+        }
+
+        /**
+         * 
+         * @param string $comment
+         * @return Error
+         */
+        function setComment($comment) {
+            $this->comment = $comment;
+            return $this;
+        }
+
 }
