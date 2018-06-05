a04c67384f03c52a607d035f25b35ec4c947d1c7
cgimazane <clement.gimazane@ign.fr>
2018-06-05 11:36:21 +0200

    #1525: add sender to ContactType + correction #1455

--- website/server/src/Ign/Bundle/GincoBundle/Form/ContactType.php
+++ website/server/src/Ign/Bundle/GincoBundle/Form/ContactType.php
@@ -3,6 +3,7 @@ namespace Ign\Bundle\GincoBundle\Form;
 
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
@@ -10,7 +11,6 @@ use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\Validator\Constraints\Blank;
 use Symfony\Component\Validator\Constraints\Email;
 use Symfony\Component\Validator\Constraints\NotBlank;
-use Symfony\Component\Validator\Constraints\Collection;
 use Symfony\Component\OptionsResolver\OptionsResolverInterface;
 
 /**
@@ -39,12 +39,68 @@ class ContactType extends AbstractType
                     new Blank(array('message' => 'Contact.validation.name.blank')),
                 ),
             ))
+            ->add('sender', TextType::class, array(
+                'label' => 'Contact.sender',
+                'constraints' => array(
+                    new NotBlank(array('message' => 'Contact.validation.sender.notblank'))
+                ),
+            ))
+            ->add('job', TextType::class, array(
+                'label' => 'Contact.job',
+                'required' => false
+            ))
             ->add('email', EmailType::class, array(
                 'label' => 'Contact.email',
                 'constraints' => array(
                     new NotBlank(array('message' => 'Contact.validation.email.notblank')),
                     new Email(array('message' => 'Contact.validation.email.invalid')),
                 ),
+            ))
+            ->add('subject', ChoiceType::class, array(
+                'label' => 'Contact.subject.title',
+                'choices' => array(
+                    'Contact.subject.legal.title' => array(
+                        'Contact.subject.legal.questions' => 'Contact.subject.legal.questions',
+                        'Contact.subject.legal.rules' => 'Contact.subject.legal.rules',
+                    ),
+                    'Contact.subject.format.title' => array(
+                        'Contact.subject.format.standard' => 'Contact.subject.format.standard',
+                        'Contact.subject.format.structure' => 'Contact.subject.format.structure',
+                        'Contact.subject.format.taxref' => 'Contact.subject.format.taxref',
+                        'Contact.subject.format.sensitivity' => 'Contact.subject.format.sensitivity',
+                    ),
+                    'Contact.subject.rules.title' => array(
+                        'Contact.subject.rules.architecture' => 'Contact.subject.rules.architecture',
+                        'Contact.subject.rules.protocol' => 'Contact.subject.rules.protocol',
+                        'Contact.subject.rules.relation' => 'Contact.subject.rules.relation',
+                        'Contact.subject.rules.validation' => 'Contact.subject.rules.validation',
+                    ),
+                    'Contact.subject.operation.title' => array(
+                        'Contact.subject.operation.organism' => 'Contact.subject.operation.organism',
+                        'Contact.subject.operation.metadata' => 'Contact.subject.operation.metadata',
+                        'Contact.subject.operation.geonature' => 'Contact.subject.operation.geonature',
+                        'Contact.subject.operation.geonatureWorkflow' => 'Contact.subject.operation.geonatureWorkflow',
+                        'Contact.subject.operation.ginco' => 'Contact.subject.operation.ginco',
+                        'Contact.subject.operation.import' => 'Contact.subject.operation.import',
+                        'Contact.subject.operation.gincoWorkflow' => 'Contact.subject.operation.gincoWorkflow'
+                    ),
+                    'Contact.subject.blocking.title' => array(
+                        'Contact.subject.blocking.ginco' => 'Contact.subject.blocking.ginco',
+                        'Contact.subject.blocking.geonature' => 'Contact.subject.blocking.geonature',
+                        'Contact.subject.blocking.metadata' => 'Contact.subject.blocking.metadata',
+                        'Contact.subject.blocking.authentication' => 'Contact.subject.blocking.authentication',
+                        'Contact.subject.blocking.sinpDirectory' => 'Contact.subject.blocking.sinpDirectory'
+                    ),
+                    'Contact.subject.evolution.title' => array(
+                        'Contact.subject.evolution.geonature' => 'Contact.subject.evolution.geonature',
+                        'Contact.subject.evolution.ginco' => 'Contact.subject.evolution.ginco',
+                        'Contact.subject.evolution.dldbb' => 'Contact.subject.evolution.dldbb',
+                        'Contact.subject.evolution.metadata' => 'Contact.subject.evolution.metadata',
+                        'Contact.subject.evolution.directory' => 'Contact.subject.evolution.directory'
+                    ),
+                    'Contact.subject.other'=> 'Contact.subject.other'
+                ),
+                'choices_as_values' => true,
             ))
             ->add('message', TextareaType::class, array(
                 'label' => 'Contact.message',
