609042735e189b11afb07e45e13d4bdb4feb42aa
amouget <anna.mouget@ign.fr>
2018-06-13 10:20:07 +0200

    #1544: Set identifiantpermanent not mandatroy in file

---
+++ database/update/v2.2.000/set_identifiantpermanent_not_mandatory_in_file.sql
@@ -0,0 +1,4 @@
+UPDATE metadata.table_field SET is_calculated=1 WHERE data = 'identifiantpermanent';
+UPDATE metadata_work.table_field SET is_calculated=1 WHERE data = 'identifiantpermanent';
+UPDATE metadata.file_field SET is_mandatory=0 WHERE data = 'identifiantpermanent';
+UPDATE metadata_work.file_field SET is_mandatory=0 WHERE data = 'identifiantpermanent';
