bb414ded111ec467dd3bd7789c765dd720ec2378
Rémi Pas <remi.pas@ign.fr>
2018-07-03 10:53:58 +0200

    1523 : add new grids to codemaillevalue

--- database/update/v2.2.000/update_db_sprint.php
+++ database/update/v2.2.000/update_db_sprint.php
@@ -30,6 +30,7 @@ try {
 	execCustSQLFile("$sprintDir/set_jddmetadonneedeeid_not_mandatory_in_file.sql", $config);
 	execCustSQLFile("$sprintDir/update_nomcommunecalcule_dynamode.sql", $config);
 	execCustSQLFile("$sprintDir/add_metadata_check.sql", $config);
+	execCustSQLFile("$sprintDir/codemaillevalue_new.sql", $config);
 
 } catch (Exception $e) {
 	echo "$sprintDir/update_db_sprint.php\n";
