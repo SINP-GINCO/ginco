43d2fabf693b669df85cffb4efb4644db62a2616
Rémi Pas <remi.pas@ign.fr>
2018-09-24 07:55:40 +0200

    fix roles

--- database/update/v2.2.001/add_developers.sql
+++ database/update/v2.2.001/add_developers.sql
@@ -1,11 +1,14 @@
 
 -- We add users on default provider because we don't want to create other providers on the platform
+DELETE FROM website.role_to_user WHERE user_login = 'xthauvin' OR user_login = 'SimonF' ;
+DELETE FROM website.users WHERE user_login = 'xthauvin' OR user_login = 'SimonF' ;
+
 INSERT INTO website.users(user_login, provider_id, email) VALUES
-('xthauvin',0,'xavier.thauvin@ign.fr'),
+  ('xthauvin',0,'xavier.thauvin@ign.fr'),
   ('SimonF',0,'simon.fauret@ign.fr')
 ;
 
 INSERT INTO website.role_to_user(user_login, role_code) VALUES
-('xthauvin',1),
- ('SimonF',1)
+  ('xthauvin', (SELECT role_code FROM website.role WHERE role_label = 'Développeur')),
+  ('SimonF', (SELECT role_code FROM website.role WHERE role_label = 'Développeur'))
 ;
