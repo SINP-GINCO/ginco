f8af53b4a846578adefcfebe7d881b3f8954ad3b
Rémi Pas <remi.pas@ign.fr>
2018-10-01 08:06:31 +0200

    [#1639] update old jdd status to validated

--- database/update/v2.2.002/remove_permissions.sql
+++ database/update/v2.2.002/remove_permissions.sql
@@ -1,2 +1,9 @@
 DELETE FROM website.permission_per_role WHERE permission_code IN ('VALIDATE_SUBMISSION_ALL', 'VALIDATE_SUBMISSION_PROVIDER', 'VALIDATE_SUBMISSION_OWN') ;
 DELETE FROM website.permission WHERE permission_code IN ('VALIDATE_SUBMISSION_ALL', 'VALIDATE_SUBMISSION_PROVIDER', 'VALIDATE_SUBMISSION_OWN') ;
+
+-- Update existing jdd status
+
+UPDATE raw_data.jdd j 
+SET status = 'validated'
+FROM raw_data.submission s
+WHERE j.id = s.jdd_id AND s.step = 'VALIDATE' AND j.status = 'active'
