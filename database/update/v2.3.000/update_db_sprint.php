bbfe14192e7b65bbd5ea30d95d3d479175841cd3
Rémi Pas <remi.pas@ign.fr>
2018-10-19 13:27:28 +0200

    prepare v2.3.0

---
+++ database/update/v2.3.000/update_db_sprint.php
@@ -0,0 +1,62 @@
+<?php
+$sprintDir = dirname(__FILE__);
+$initDir = realpath(dirname(__FILE__)."/../../init/") ;
+require_once "$sprintDir/../../../lib/share.php";
+
+// ------------------------------------------------
+// Synopsis: migrate DB GINCO from v2.2.2 to v2.3.0
+// ------------------------------------------------
+function usage($mess = NULL) {
+	echo "------------------------------------------------------------------------\n";
+	echo ("\nUpdate DB from v2.2.2 to v2.3.0\n");
+	echo ("> php update_db_sprint.php -f <configFile> [{-D<propertiesName>=<Value>}]\n\n");
+	echo "o <configFile>: a java style properties file for the instance on which you work\n";
+	echo "o -D : inline options to complete or override the config file.\n";
+	echo "------------------------------------------------------------------------\n";
+	if (!is_null($mess)) {
+		echo ("$mess\n\n");
+		exit(1);
+	}
+	exit();
+}
+
+if (count($argv) == 1)
+	usage();
+$config = loadPropertiesFromArgs();
+
+$isDlb = strpos($config['db.name'], 'dlb') !== FALSE ;
+if ($isDlb) {
+	echo "Database is DLB." ;
+}
+
+try {
+	/* patch code here*/
+	//execCustSQLFile("$sprintDir/xxxx.sql", $config);
+
+
+} catch (Exception $e) {
+	echo "$sprintDir/update_db_sprint.php\n";
+	echo "exception: " . $e->getMessage() . "\n";
+	exit(1);
+}
+
+$CLIParams = implode(' ', array_slice($argv, 1));
+/* patch php here */
+
+try {
+
+	//system("php $sprintDir/XXXX.php $CLIParams", $returnCode1);
+	
+
+} catch (Exception $e) {
+	
+	echo "$sprintDir/update_db_sprint.php : an exception has occured.\n" ;
+	echo "Exception : {$e->getMessage()}" ;
+	exit(1) ;
+}
+
+
+
+
+
+
