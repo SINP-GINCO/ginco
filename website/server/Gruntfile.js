a94d4cbaeb798bce2d26dbb20316461e6dc11c60
Rémi Pas <remi.pas@ign.fr>
2018-11-07 15:30:20 +0100

    [#1537] use npm + grunt instead of assetic

---
+++ website/server/Gruntfile.js
@@ -0,0 +1,71 @@
+module.exports = function (grunt) {
+    // Project configuration.
+    grunt.initConfig({
+
+        less: {
+            bootstrap: {
+                options: {
+                    cleancss: true
+                },
+                files: {
+                    "web/css/bootstrap.css" : "app/Resources/less/style.less"
+                }
+            }
+        },
+
+        copy: {
+            jquery: {
+                files: [{
+                        expand: true,
+                        cwd: 'node_modules/jquery',
+                        src: ['dist/**'],
+                        dest: 'web/vendor/jquery'
+                    }]
+            },
+            jqueryui: {
+                files: [{
+                    expand: true,
+                    cwd: 'node_modules/components-jqueryui',
+                    src: ['**.js', 'themes/base/*.css'],
+                    dest: 'web/vendor/components-jqueryui'
+                }]
+            },
+            bootstrap: {
+                files: [{
+                        expand: true,
+                        cwd: 'node_modules/bootstrap',
+                        src: ['dist/**'],
+                        dest: 'web/vendor/bootstrap'
+                    }]
+            },
+            fonts: {
+                files: [{
+                    expand: true,
+                    cwd: 'node_modules/bootstrap/fonts',
+                    src: ['**'],
+                    dest: 'web/css/fonts'
+                }]
+            },
+            bootstrapTable: {
+                files: [{
+                        expand: true,
+                        cwd: 'node_modules/bootstrap-table',
+                        src: ['dist/**'],
+                        dest: 'web/vendor/bootstrap-table'
+                    }]
+            },
+            ginco: {
+                files: [{
+                    expand: true,
+                    cwd: 'app/Resources/',
+                    src: ['js/**.js', 'css/**.css'],
+                    dest: 'web/'
+                }]
+            }          
+        }
+    });
+
+    grunt.loadNpmTasks('grunt-contrib-copy');
+    grunt.loadNpmTasks('grunt-contrib-less');
+    grunt.registerTask('default', ['less', 'copy']);
+};
