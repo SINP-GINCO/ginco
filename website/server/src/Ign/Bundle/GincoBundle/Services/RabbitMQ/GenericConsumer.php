46597f3293ea4f6ad5bc469957d9661d8ecbf5c9
Rémi Pas <remi.pas@ign.fr>
2018-12-06 09:38:10 +0100

    fix

--- website/server/src/Ign/Bundle/GincoBundle/Services/RabbitMQ/GenericConsumer.php
+++ website/server/src/Ign/Bundle/GincoBundle/Services/RabbitMQ/GenericConsumer.php
@@ -85,7 +85,7 @@ class GenericConsumer implements ConsumerInterface {
 			// Get Message entity;
 			$messageId = $data['message_id']; // Message id in messages table
 			echo $messageId;
-			$message = $this->em->getRepository('IgnGincoBundle:Website\Message')->findOneById($messageId);
+			$message = $this->em->getRepository('IgnGincoBundle:Website\Message')->find($messageId);
 			if (!$message) {
 				echo $this->datelog() . "Received non existant message id $messageId.\n" ;
 				return false ;
