a38b39a0502b783532de14ace6e3f2119d58f29f
Rémi Pas <remi.pas@ign.fr>
2018-12-11 13:22:57 +0100

    move services into bundle configuration

--- website/server/src/Ign/Bundle/GincoBundle/DependencyInjection/IgnGincoExtension.php
+++ website/server/src/Ign/Bundle/GincoBundle/DependencyInjection/IgnGincoExtension.php
@@ -25,5 +25,7 @@ class IgnGincoExtension extends Extension
         $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
         $loader->load('services.yml');
 		$loader->load('voters.yml') ;
+		$loader->load('services_ginco.yml') ;
+		$loader->load('services_configurator.yml') ;
     }
 }
