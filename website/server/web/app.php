fce4039c5c181339ea726a803bd4222236994609
Rémi Pas <remi.pas@ign.fr>
2018-12-12 11:44:50 +0100

    fix trusted proxies configuration

--- website/server/web/app.php
+++ website/server/web/app.php
@@ -7,6 +7,16 @@ $loader = require __DIR__.'/../app/autoload.php';
 
 $kernel = new AppKernel('prod', false);
 
+/* trusted_proxies to get ip & co from load balancer */
+Request::setTrustedProxies(['10.0.0.0/8'], Request::HEADER_X_FORWARDED_ALL);
+
+/* spécification des entêtes pour la récupération des informations derrière un proxy (patch ATOS) */
+Request::setTrustedHeaderName(Request::HEADER_FORWARDED, null);
+Request::setTrustedHeaderName(Request::HEADER_CLIENT_IP, 'X-Forwarded-For');
+Request::setTrustedHeaderName(Request::HEADER_CLIENT_HOST, 'X-Forwarded-Host');
+Request::setTrustedHeaderName(Request::HEADER_CLIENT_PORT, 'X-Forwarded-Port');
+Request::setTrustedHeaderName(Request::HEADER_CLIENT_PROTO, 'X-Forwarded-Proto');
+
 // When using the HttpCache, you need to call the method in your front controller instead of relying on the configuration parameter
 //Request::enableHttpMethodParameterOverride();
 $request = Request::createFromGlobals();
