209b4ca5949aa4dad4f5de23e9a79e7b4e2ee517
Rémi Pas <remi.pas@ign.fr>
2019-05-22 08:40:35 +0200

    [#1741] fix import from metadata_work, do not import FORM stuff

--- database/update/v2.3.000/insert_metadata_work.php
+++ database/update/v2.3.000/insert_metadata_work.php
@@ -11,27 +11,30 @@ $pdo = new PDO($dsn, $config['db.adminuser'], $config['db.adminuser.pw']) ;
 $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION) ;
 
 $tables = array(
-    'data'              => ['data'],
-    'format'            => ['format'],
-    'field'             => ['data', 'format'],
+    //'data'              => ['data'],
+    //'format'            => ['format'],
+    //'field'             => ['data', 'format'],
     'table_format'      => ['format'],
     'file_format'       => ['format'],
-    'form_format'       => ['format'],
+    //'form_format'       => ['format'],
+    'model_tables'      => ['model_id', 'table_id'],  
+    //'field_mapping'     => ['src_data', 'src_format', 'dst_data', 'dst_format'],
+    //'form_field'        => ['data', 'format'],
+    'table_field'       => ['data', 'format'],
+    'file_field'        => ['data', 'format']
+);
+
+$datasetTables = array(
     'dataset_forms'     => ['dataset_id', 'format'],
     'model_datasets'    => ['model_id', 'dataset_id'],
     'dataset_files'     => ['dataset_id', 'format'],
-    'model_tables'      => ['model_id', 'table_id'],
     'dataset_fields'    => ['dataset_id', 'format', 'data'],
-    'field_mapping'     => ['src_data', 'src_format', 'dst_data', 'dst_format'],
-    'form_field'        => ['data', 'format'],
-    'table_field'       => ['data', 'format'],
-    'file_field'        => ['data', 'format']
 );
 
 
 try {
 
-    $pdo->beginTransaction() ;
+    //$pdo->beginTransaction() ;
 
     // Insertion des modèles, non publié.
     $sql = "INSERT INTO metadata.model (
@@ -51,11 +54,49 @@ try {
                 FROM metadata_work.dataset WHERE dataset_id IN (
                     SELECT dataset_id FROM metadata_work.dataset WHERE type != 'QUERY'
                     EXCEPT
-                    SELECT dataset_id FROM metadata.dataset WHERE type != 'QUERY'
+                    SELECT dataset_id FROM metadata.dataset
                 )
             )
     ";
     $pdo->exec($sql) ;
+
+    // Insertion des data
+    $sql = "INSERT INTO metadata.data (
+            SELECT *
+            FROM metadata_work.data WHERE data IN (
+                SELECT data FROM metadata_work.data
+                EXCEPT
+                SELECT data FROM metadata.data
+            )
+        )
+    ";
+    $pdo->exec($sql) ;
+
+    // Insertion des format, sauf ceux des formulaires.
+    $sql = "INSERT INTO metadata.format (
+            SELECT *
+            FROM metadata_work.format WHERE format IN (
+                SELECT format FROM metadata_work.format WHERE type != 'FORM'
+                EXCEPT
+                SELECT format FROM metadata.format
+            )
+        )
+    ";
+    $pdo->exec($sql) ;
+
+    
+    // Insertion des field, sauf ceux des formulaires.
+    $sql = "INSERT INTO metadata.field (
+            SELECT *
+            FROM metadata_work.field WHERE (data, format) IN (
+                SELECT data,format FROM metadata_work.field WHERE type != 'FORM'
+                EXCEPT
+                SELECT data,format FROM metadata.field
+            )
+        )
+    ";
+    $pdo->exec($sql) ;
+
 
     foreach ($tables as $table => $ids) {
 
@@ -72,6 +113,35 @@ try {
         $pdo->exec($sql) ; 
     }
 
+    foreach ($datasetTables as $table => $ids) {
+
+        $implodedIds = implode(',', $ids) ;
+        $sql = "INSERT INTO metadata.$table (
+                    SELECT *
+                    FROM metadata_work.$table WHERE ($implodedIds) IN (
+                        SELECT $implodedIds FROM metadata_work.$table
+                        INNER JOIN metadata_work.dataset d USING (dataset_id)
+                        WHERE d.type != 'QUERY'
+                        EXCEPT
+                        SELECT $implodedIds FROM metadata.$table
+                    )
+            )
+        ";
+        $pdo->exec($sql) ; 
+    }
+
+    // Insertion des field_mapping, sauf ceux des formulaires.
+    $sql = "INSERT INTO metadata.field_mapping (
+            SELECT *
+            FROM metadata_work.field_mapping WHERE (src_data, src_format, dst_data, dst_format) IN (
+                SELECT src_data, src_format, dst_data, dst_format FROM metadata_work.field_mapping WHERE mapping_type != 'FORM'
+                EXCEPT
+                SELECT src_data, src_format, dst_data, dst_format FROM metadata.field_mapping
+            )
+        )
+    ";
+    $pdo->exec($sql) ;
+
     // table_tree : remplacement * par NULL.
     $sql = "INSERT INTO metadata.table_tree (
             SELECT schema_code, child_table, NULL, join_key, comment
@@ -83,11 +153,11 @@ try {
         )
     ";
     
-    $pdo->commit() ;
+    //$pdo->commit() ;
     
 } catch (PDOException $e) {
     
-    $pdo->rollBack() ;
+    //$pdo->rollBack() ;
 	echo "$sprintDir/update_db_sprint.php\n";
 	echo "exception: " . $e->getMessage() . "\n";
 	exit(1);
