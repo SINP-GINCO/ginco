1cd433d4f5479abf89a2c6a1ca08de71700e7f28
Rémi Pas <remi.pas@ign.fr>
2019-10-21 14:44:17 +0200

    [#1791] add control on existence of cleStation

--- database/update/v2.4.000/add_checks.sql
+++ database/update/v2.4.000/add_checks.sql
@@ -4,5 +4,6 @@ INSERT INTO metadata.checks (check_id, step, name, label, description, importanc
    (1214, 'CONFORMITY', 'CDHAB_EMPTY', 'cdHab est vide', 'cdHab ne doit pas être vide si nomCite est Inconnu ou Nom perdu', 'ERROR'),
    (1215, 'CONFORMITY', 'PRECISION_TECHNIQUE_EMPTY', 'precisionTechnqiue est vide', 'precisionTechnique ne doit pas être vide si techniqueCollecte = 10', 'ERROR'),
    (1216, 'CONFORMITY', 'CDHAB_INTERET_COMMUNAUTAIRE_EMPTY', 'cdHabInteretCommunautaire est vide', 'cdHabInteretCommunautaire ne doit pas être vide si habitatInteretCommunautaire est égal à 1 ou 3', 'ERROR'),
-   (1217, 'CONFORMITY', 'IDENTIFIANT_PERMANENT_NOT_UNIQUE', 'L''identifiant permanent fourni existe déjà', 'L''identifiant permanent fourni existe déjà', 'ERROR')
+   (1217, 'CONFORMITY', 'IDENTIFIANT_PERMANENT_NOT_UNIQUE', 'L''identifiant permanent fourni existe déjà', 'L''identifiant permanent fourni existe déjà', 'ERROR'),
+   (1218, 'CONFORMITY', 'CLE_STATION_NOT_FOUND', 'La clé station fournie pour les habitats n''existe pas', 'La clé station fournie pour les habitats n''existe pas', 'ERROR')
 ;
